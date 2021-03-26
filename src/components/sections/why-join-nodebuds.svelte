8baad7b623e75363a02d083227fab3cfd1f2d056
BJ Warfield <bjwarfield83@gmail.com>
2021-03-26 15:55:24 -0700

    Update nodeBuds path
    added why-join-nodeBuds component
    added how-can-i-get-involved component
    modified nodebuds path to contain these components

---
+++ src/components/sections/why-join-nodebuds.svelte
@@ -0,0 +1,81 @@
+<div class="turnback">
+  <div class="why-join-nodebuds">
+    <!-- <img src="../assets/png/why-join-node-buds-illustration.png" alt="" /> -->
+
+    <object
+      type="image/png"
+      data="../assets/png/why-join-node-buds-illustration.png"
+      title="Why Join nodeBuds"></object>
+    <div>
+      <h2>why join node<span class="brand-red">buds</span>?</h2>
+      <p>
+        <span class="brand-em">node<span class="brand-red">Buds</span></span> was
+        designed to provide underclassmen and transfer students assistance as they
+        start their fresh journey through CSUF, and through the tech industry. We
+        provide the knowledge and resources that’ll help students start on the right
+        foot.
+      </p>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  @import "../../style/theme.scss";
+
+  .turnback {
+    div {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      align-items: center;
+      padding: 4em 2em 0;
+
+      &:nth-child(even) {
+        flex-flow: row-reverse;
+        div {
+          align-items: flex-start;
+          p {
+            text-align: left;
+          }
+        }
+      }
+
+      @media screen and (max-width: $breakpoint) {
+        flex-direction: column;
+        align-items: center;
+        flex-flow: column !important;
+        div {
+          justify-content: center;
+          align-items: center;
+          p,
+          h2 {
+            width: 100%;
+            text-align: center !important;
+          }
+        }
+      }
+
+      object {
+        @include fluidSize(237, 932, $propName: "width");
+        @include fluidSize(80, 314, $propName: "height");
+      }
+      div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-end;
+        flex-flow: column !important;
+        h2 {
+          @include fluidSize($minHFontSize, $maxHFontSizeMed);
+          text-align: center;
+          font-weight: bold;
+        }
+        p {
+          @include fluidSize($minPFontSize, $maxPFontSize);
+          @include fluidSize(150, 800, $propName: "width");
+          text-align: right;
+        }
+      }
+    }
+  }
+</style>
--- src/routes/node-buds/index.svelte
+++ src/routes/node-buds/index.svelte
@@ -1,5 +1,55 @@
 <script lang="ts">
-  import ComingSoon from "@/components/sections/coming-soon.svelte";
+  // import ComingSoon from "@/components/sections/coming-soon.svelte";
+  import Headline from "@/components/sections/headline.svelte";
+  import WhyJoinNodeBuds from "@/components/sections/why-join-nodebuds.svelte";
+  // import WhatAreYouWaitingFor from "@/components/sections/what-are-you-waiting-for.svelte";
+  import HowCanIGetInvoved from "@/components/sections/how-can-i-get-involved.svelte";
+  import WhyJoinNodebuds from "../../components/sections/why-join-nodebuds.svelte";
 </script>
 
-<ComingSoon title="node buds" src="../assets/badges/PNG/node-buds-badge.png" />
+<!-- <ComingSoon title="node buds" src="../assets/badges/PNG/node-buds-badge.png" /> -->
+<section class="nodebuds-screen-hero">
+  <div>
+    <Headline src="../assets/badges/PNG/node-buds-badge.png"
+      >node<span class="brand-red">Buds</span>
+    </Headline>
+    <div class="about-description-container">
+      <h2 class="brand-em">what is node<span class="brand-red">Buds</span>?</h2>
+      <p>
+        <span class="brand-em">node<span class="brand-red">Buds</span></span> is
+        a mentorship program that aims to help underclassmen adjust to university
+        life, and to prepate them for the tech industry. We achieve this by having
+        upper an underclassman form strong connection with each other which allows
+        them to exchange advice, skills, and more, all in order to start student
+        off successfully.
+      </p>
+    </div>
+  </div>
+</section>
+
+<WhyJoinNodebuds />
+<HowCanIGetInvoved />
+
+<style lang="scss">
+  @import "../../style/theme.scss";
+  .nodebuds-screen-hero {
+    padding-top: 100px;
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      h2 {
+        @include fluidSize($minHFontSize, $maxHFontSizeMed);
+        text-align: center;
+        font-weight: bold;
+        padding: 1em 0;
+      }
+      p {
+        @include fluidSize($minPFontSize, $maxPFontSize);
+        @include fluidSize(220, 1500, $propName: "width");
+        text-align: center;
+      }
+    }
+  }
+</style>
