42a2962223f5d0ef76edd90f50cf758a286fa98f
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-04-08 23:19:54 -0700

    Created the `officer-profile-list.svelte` component and more.
    I created the necessary `officer-profile-list.svelte` as well as the `acm-select.svelte` UI component to go with it.

---
+++ src/components/sections/officer-profile-list.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { styleProps } from "../../actions/use-style-props";
+  import { officers } from "../../lib/officers";
+  import OfficerProfile from "@/components/sections/officer-profile.svelte";
+  import AcmSelect from "@/components/ui/acm-select.svelte";
+
+  let currentSchoolYear = "2020-2021";
+</script>
+
+<section>
+  <h2>
+    who are
+    <span use:styleProps="{{ 'emphasis-color': 'var(--acm-blue)' }}"> we </span>
+  </h2>
+  <div class="school-year-input-container">
+    <AcmSelect
+      bind:defaultValue="{currentSchoolYear}"
+      options="{Object.keys(officers)}"
+    />
+  </div>
+  <div class="officer-profile-list">
+    {#each officers[currentSchoolYear] as { name, title, picture } (name)}
+      <OfficerProfile
+        name="{name}"
+        title="{title}"
+        picture="{picture}"
+        key="{picture}"
+      />
+    {/each}
+  </div>
+</section>
+
+<style>
+  section h2 {
+    text-align: center;
+    font-weight: bold;
+    font-size: 3rem;
+  }
+  span {
+    color: var(--emphasis-color);
+  }
+  .school-year-input-container {
+    min-width: 100px;
+    text-align: center;
+  }
+  .officer-profile-list {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+  }
+</style>
---
+++ src/components/ui/acm-select.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let defaultValue: string = "";
+  export let options: string[] = [];
+</script>
+
+<select name="school-year" bind:value="{defaultValue}">
+  {#each options as optionValue (optionValue)}
+    <option value="{optionValue}">{optionValue}</option>
+  {/each}
+</select>
+
+<style>
+  select {
+    background-color: var(--acm-dark);
+    padding: 1em;
+    color: var(--acm-light);
+    font-family: "Poppins", sans-serif;
+    font-weight: bold;
+    border-radius: 25px;
+  }
+
+  option {
+    background-color: var(--acm-dark);
+    color: var(--acm-light);
+    border-radius: 25px;
+  }
+</style>
