1bf4961b34369e20e8c27f782bf027a283bcb807
Ethan <ethan.r.davidson@gmail.com>
2021-04-25 14:29:49 -0700

    Implemented scrolling the carousel via grabbing ✊
    When the user clicks and moves their cursor along the event carousel component, the scroll offset will automatically increment. Below is an emoji diagram of the expected gesture.
    
    ```
    🖐✊➡🖐
    ```

--- src/components/events/event-carousel.svelte
+++ src/components/events/event-carousel.svelte
@@ -1,13 +1,52 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import EventItem from "./event-item.svelte";
   import type { AcmEvent } from "../../lib/parse-ical-data";
 
+  enum GrabbingState {
+    Active = "true",
+    Inactive = "false",
+  }
+
   export let events: AcmEvent[] = [];
+
+  let carouselRef: HTMLDivElement;
+  const scrollSpeed = 2;
+
+  const scrollTheCarousel = (movementScalar: number) => {
+    carouselRef.scrollBy({
+      left: -movementScalar * scrollSpeed,
+    });
+  };
+
+  const scrollOnMouseMove = (event: MouseEvent) => {
+      if (carouselRef.getAttribute("data-grabbing") === GrabbingState.Active) {
+        scrollTheCarousel(event.movementX);
+      }
+    },
+    startGrabbing = () => {
+      carouselRef.setAttribute("data-grabbing", GrabbingState.Active);
+    },
+    endGrabbing = () => {
+      carouselRef.setAttribute("data-grabbing", GrabbingState.Inactive);
+    };
+
+  onMount(() => {
+    carouselRef.setAttribute("data-grabbing", GrabbingState.Inactive);
+    carouselRef.addEventListener("mousemove", scrollOnMouseMove);
+    carouselRef.addEventListener("mousedown", startGrabbing);
+    carouselRef.addEventListener("mouseup", endGrabbing);
+    return () => {
+      carouselRef.removeEventListener("mousemove", scrollOnMouseMove);
+      carouselRef.removeEventListener("mousedown", startGrabbing);
+      carouselRef.removeEventListener("mouseup", endGrabbing);
+    };
+  });
 </script>
 
 <section>
   <h2>this week's events</h2>
-  <div>
+  <div bind:this="{carouselRef}" data-grabbing="{GrabbingState.Active}">
     {#each events as eventInfo}
       <EventItem info="{eventInfo}" />
     {/each}
@@ -25,10 +64,15 @@
     margin-bottom: 40px;
   }
 
+  section div[data-grabbing="true"] {
+    cursor: grabbing;
+  }
+
   section div {
     display: flex;
     flex-direction: row;
     overflow-x: scroll;
-    padding-bottom: 60px;
+    padding-bottom: 32px;
+    cursor: grab;
   }
 </style>
--- src/components/events/event-item.svelte
+++ src/components/events/event-item.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import type { AcmEvent } from "../../lib/parse-ical-data";
+  import { links } from "../../lib/links";
   import AcmButton from "@/components/utils/acm-button.svelte";
   export let info: AcmEvent;
 
   const isDiscordEvent = info.location.length === 0;
-  const meetingLink = isDiscordEvent ? "discord_link_here" : info.location;
+  const meetingLink = isDiscordEvent ? links.discord : info.location;
 </script>
 
 <div class="event-card">
@@ -30,6 +31,7 @@
     justify-content: space-between;
     align-items: center;
     width: 320px;
+    min-width: 320px;
     margin: 0 15px;
     padding: 20px;
     box-shadow: 0px 12px 18px rgba(55, 146, 193, 0.5);
@@ -41,10 +43,12 @@
     line-height: var(--subheading-font-size);
     margin: 10px 0;
     text-align: center;
+    user-select: none;
   }
 
   .event-card p {
     font-size: calc(var(--body-font-size) * 0.75);
     text-align: center;
+    user-select: none;
   }
 </style>
