0ab22776078ba25432995cc3810eeeec0e0fea87
Ethan <ethan.r.davidson@gmail.com>
2021-04-25 14:52:38 -0700

    Update footer.svelte
    The footer had the incorrect image attached, so I fixed it. The image now correctly points to `/assets/png/acm-csuf-badge.png`.

--- src/components/sections/footer.svelte
+++ src/components/sections/footer.svelte
@@ -1,15 +1,7 @@
 <footer>
   <span class="year">&copy; 2021</span>
-  <img src="assets/badges/acm-csuf-badge.svg" width="48" class="acm-logo" />
+  <img src="assets/png/acm-csuf-badge.png" width="48" class="acm-logo" />
   <span class="brand-em">CSUF</span>
-  <!-- <a href="/">
-    <object
-      type="image/png"
-      data="assets/badges/PNG/acm-csuf-badge.png"
-      title="acm-csuf-logo"
-    />
-  </a>
-  <span class="brand-em">CSUF</span> -->
 </footer>
 
 <style>
