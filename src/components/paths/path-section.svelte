20c03c4ed709d54a909709b52f2e76933f367f4b
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-04-26 00:37:48 -0700

    Created `path-section.svelte` to aide in rewriting the the `/paths` route.
    Created a new component, `path-section.svelte`, and used it in the `/paths` route for each acmCSUF path.

---
+++ src/components/paths/path-section.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+  import type { AcmPath } from "../../lib/acm-paths";
+  import { styleProps } from "../../actions/use-style-props";
+
+  enum TextAlignment {
+    Left = "left",
+    Right = "right",
+  }
+
+  export let textAlign: TextAlignment = TextAlignment.Right;
+  export let info: AcmPath | undefined;
+</script>
+
+{#if info !== undefined}
+  <section id="{info.slug}" class:👈="{textAlign === TextAlignment.Left}">
+    <img src="{info.picture}" alt="{`acm${info.title} Logo`}" />
+    <div>
+      <h2>
+        <span class="brand-em">
+          acm<span use:styleProps="{{ 'font-color': info.color }}">
+            {info.title}
+          </span>
+        </span>
+      </h2>
+      <slot name="content" tag="p" />
+    </div>
+  </section>
+{/if}
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-top: calc(1.5 * var(--navbar-height));
+    margin-top: calc(-1.5 * var(--navbar-height));
+  }
+
+  section img {
+    margin-bottom: 32px;
+    width: 200px;
+    height: 200px;
+  }
+
+  section div {
+    display: flex;
+    flex-direction: column;
+    margin: 0 32px;
+  }
+
+  section div h2 {
+    font-size: var(--heading-font-size);
+    text-align: right;
+  }
+
+  /* Class `.👈` represents `<PathSection textAlign="left" />` */
+  .👈 div h2 {
+    text-align: left;
+  }
+
+  section div h2 span span {
+    color: var(--font-color);
+  }
+
+  section div :global(p) {
+    font-size: var(--body-font-size);
+    text-indent: 32px;
+    text-align: right;
+  }
+
+  .👈 div :global(p) {
+    text-align: left;
+  }
+
+  @media screen and (min-width: 768px) {
+    section {
+      flex-direction: row;
+      margin: 0 32px;
+    }
+
+    .👈 {
+      flex-direction: row-reverse;
+    }
+
+    section img {
+      width: 316px;
+      height: 316px;
+    }
+
+    section div {
+      max-width: 720px;
+    }
+  }
+</style>
--- src/routes/paths.svelte
+++ src/routes/paths.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import { acmAlgo, acmCreate, acmDev } from "../lib/acm-paths";
   import CommonHero from "@/components/sections/common-hero.svelte";
+  import PathSection from "@/components/paths/path-section.svelte";
   import Spacing from "@/components/sections/spacing.svelte";
   import CallToActionSection from "@/components/sections/call-to-action-section.svelte";
 </script>
@@ -15,59 +17,36 @@
     and communication to prepare people for real-world opportunities in tech.
   </p>
 </CommonHero>
-
 <Spacing />
-
-<section class="paths-screen-info turnback">
-  <div id="acmalgo">
-    <img src="../assets/png/acm-algo-badge.png" alt="acmAlgo Logo" />
-    <div>
-      <h2>
-        <span class="brand-em">acm<span class="brand-purple">Algo</span></span>
-      </h2>
-      <p>
-        <span class="brand-em">acm<span class="brand-purple">Algo</span></span> is
-        our committee dedicated to building the programming proficiency of students.
-        This committee focuses on increasing knowledge and understanding of data
-        structures/algorithms, enhancing problem-solving abilities, and exploring
-        competitve programing.
-      </p>
-    </div>
-  </div>
-  <div id="acmcreate">
-    <img src="../assets/png/acm-create-badge.png" alt="acmCreate Logo" />
-    <div>
-      <h2>
-        <span class="brand-em">acm<span class="brand-pink">Create</span></span>
-      </h2>
-      <p>
-        <span class="brand-em">acm<span class="brand-pink">Create</span></span> is
-        our committee dedicated to emphasizing the importance of product design.
-        This committee focuses on designing the products of acmCSUF, educating students
-        about the princples of UI/UX design, and exposing people to both technical
-        and non-technical roles in the industry.
-      </p>
-    </div>
-  </div>
-  <div id="acmdev">
-    <img src="../assets/png/acm-dev-badge.png" alt="acmDev Logo" />
-    <div>
-      <h2>
-        <span class="brand-em">acm<span class="brand-bluer">Dev</span></span>
-      </h2>
-      <p>
-        <span class="brand-em">acm<span class="brand-bluer">Dev</span></span> is
-        our committee dedicated to bringing students of all skill-levels together
-        through the use of hands-on projects and activities. This committee introduces
-        students to the software development process, to the different types of technologies
-        used by industry, and enhances people’s passion in STEM.
-      </p>
-    </div>
-  </div>
-</section>
-
+<PathSection info="{acmAlgo}" textAlign="right">
+  <p slot="content">
+    <span class="brand-em">acm<span class="brand-purple">Algo</span></span> is our
+    committee dedicated to building the programming proficiency of students. This
+    committee focuses on increasing knowledge and understanding of data structures/algorithms,
+    enhancing problem-solving abilities, and exploring competitve programing.
+  </p>
+</PathSection>
+<Spacing />
+<PathSection info="{acmCreate}" textAlign="left">
+  <p slot="content">
+    <span class="brand-em">acm<span class="brand-pink">Create</span></span> is our
+    committee dedicated to emphasizing the importance of product design. This committee
+    focuses on designing the products of acmCSUF, educating students about the princples
+    of UI/UX design, and exposing people to both technical and non-technical roles
+    in the industry.
+  </p>
+</PathSection>
+<Spacing />
+<PathSection info="{acmDev}" textAlign="right">
+  <p slot="content">
+    <span class="brand-em">acm<span class="brand-bluer">Dev</span></span> is our
+    committee dedicated to bringing students of all skill-levels together through
+    the use of hands-on projects and activities. This committee introduces students
+    to the software development process, to the different types of technologies used
+    by industry, and enhances people’s passion in STEM.
+  </p>
+</PathSection>
 <Spacing amount="175px" />
-
 <CallToActionSection>
   <h2 slot="headline">how can i get involved?</h2>
   <p slot="text">
@@ -76,76 +55,4 @@
     part of our chapter to start off your tech journey on the right foot.
   </p>
 </CallToActionSection>
-
 <Spacing amount="175px" />
-
-<style>
-  .turnback div {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    padding-top: 100px;
-  }
-
-  .turnback div h2 {
-    padding-bottom: 48px;
-    font-size: var(--subheading-font-size);
-  }
-
-  .turnback div p {
-    font-size: var(--body-font-size);
-  }
-
-  .turnback:nth-child(even) {
-    flex-flow: row-reverse;
-  }
-
-  .turnback:nth-child(even) div {
-    align-items: flex-start;
-  }
-
-  .turnback:nth-child(even) p {
-    text-align: left;
-  }
-
-  @media screen and (max-width: 756px) {
-    .turnback {
-      flex-direction: column;
-      align-items: center;
-      flex-flow: column !important;
-    }
-
-    .turnback div {
-      justify-content: center;
-      align-items: center;
-    }
-
-    .turnback div p,
-    .turnback div h2 {
-      width: 100%;
-      text-align: center !important;
-    }
-
-    .turnback div img {
-      width: 80px;
-      height: 80px;
-    }
-
-    .turnback div div {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: flex-end;
-      flex-flow: column !important;
-    }
-
-    .turnback div h2 {
-      text-align: center;
-      font-weight: bold;
-    }
-    .turnback div p {
-      text-align: right;
-    }
-  }
-</style>
