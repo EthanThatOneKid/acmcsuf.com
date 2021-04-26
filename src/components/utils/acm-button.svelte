980f404d3d3f1f3c732c2cd4dcc63ec564d27b32
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2021-04-26 01:05:01 -0700

    Revert "Event Page Progress 🏔"

--- src/components/utils/acm-button.svelte
+++ src/components/utils/acm-button.svelte
@@ -3,15 +3,13 @@
   export let text: string = "Submit";
 </script>
 
-<a class="call-to-action" href="{link}">{text}</a>
+<a class="acm-button" href="{link}" target="_blank">{text}</a>
 
 <style>
-  .call-to-action {
-    display: block;
-    text-align: center;
+  .acm-button {
     cursor: pointer;
-    border-radius: 15px;
     user-select: none;
+    border-radius: 25px;
     background-color: var(--acm-dark);
     padding: 8px 16px 8px 16px;
     margin-top: 24px;
@@ -19,13 +17,19 @@
     color: var(--acm-light);
     text-decoration: none;
     border: 2px solid var(--acm-dark);
-    font-size: var(--body-font-size);
+    font-size: 1rem;
     text-transform: lowercase;
   }
 
-  .call-to-action:hover {
+  .acm-button:hover {
     transition: color 0.25s ease-in-out, background-color 0.25s ease-in-out;
     color: var(--acm-blue);
     background-color: var(--acm-light);
+  }
+
+  @media (min-width: 768px) {
+    .acm-button {
+      font-size: 2rem;
+    }
   }
 </style>
