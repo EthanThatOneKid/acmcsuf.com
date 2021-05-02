f47bc2e87cfc811b959d639ddafe92b45aa79fad
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-05-02 16:34:27 -0700

    Shortened caching mechanism for the events endpoint.
    I changed it from 30 minutes to 2 minutes because 30 minutes is a bit too long as far as throttling calendar updates goes.

--- src/routes/events.json.ts
+++ src/routes/events.json.ts
@@ -6,7 +6,7 @@ import fetch from "node-fetch";
 const ICAL_TARGET_URL =
     "https://calendar.google.com/calendar/ical/738lnit63cr2lhp7jtduvj0c9g%40group.calendar.google.com/public/basic.ics";
 
-const expirationTimeout = 1e3 * 60 * 30; // Fetch updates every 30 minutes.
+const expirationTimeout = 1e3 * 60 * 3; // Fetch updates every 2 minutes.
 let eventExpirationTimestamp = 0;
 let events: AcmEvent[] = [];
 
