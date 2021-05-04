e41196e59ebafacdf02d94f52dba52860188ddbc
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-05-03 17:04:55 -0700

    Updated `acm-button.svelte` component styling

--- src/components/utils/acm-button.svelte
+++ src/components/utils/acm-button.svelte
@@ -12,20 +12,25 @@
     cursor: pointer;
     border-radius: 15px;
     user-select: none;
-    background-color: var(--acm-dark);
+    background-color: none;
     padding: 8px 16px 8px 16px;
     margin-top: 24px;
     font-weight: bold;
-    color: var(--acm-light);
+    color: var(--acm-dark);
     text-decoration: none;
     border: 2px solid var(--acm-dark);
     font-size: var(--body-font-size);
     text-transform: lowercase;
+    box-shadow: 0 0 0 rgba(23, 26, 28, 0.1);
+    transition: background-color 0.25s ease-in-out, color 0.25s ease-in-out,
+      -webkit-filter 0.25s ease-in-out, filter 0.25s ease-in-out;
   }
 
   .call-to-action:hover {
     transition: color 0.25s ease-in-out, background-color 0.25s ease-in-out;
-    color: var(--acm-blue);
-    background-color: var(--acm-light);
+    color: var(--acm-light);
+    background-color: var(--acm-dark);
+    -webkit-filter: drop-shadow(0 2px 10px rgba(23, 26, 28, 0.5));
+    filter: drop-shadow(0 2px 10px rgba(23, 26, 28, 0.5));
   }
 </style>
