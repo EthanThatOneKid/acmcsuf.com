4f67fbe5437ddecb18bf876bb68fc9ff83e71ea9
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2021-05-04 17:48:25 -0700

    Update event-carousel.svelte

--- src/components/events/event-carousel.svelte
+++ src/components/events/event-carousel.svelte
@@ -43,7 +43,7 @@
 </script>
 
 <section>
-  <h2>This week's events</h2>
+  <h2>Upcoming events</h2>
   <div class="event-carousel-container">
     <div bind:this="{carouselButtonLeft}" class="carousel-button left">
       &lt;
