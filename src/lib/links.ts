d66e3040403d535b3630127134b146755e3ddf2f
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2021-05-14 12:24:47 -0700

    Update links.ts

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,4 +1,5 @@
 export const links = {
   'discord': "https://discord.com/invite/Dfssdtd",
-  'github': "https://github.com/EthanThatOneKid/acmcsuf.com"
+  'github': "https://github.com/EthanThatOneKid/acmcsuf.com",
+  'youtube': "https://www.youtube.com/channel/UCDMBj0o4V8Cpt0jgsZLNVVg"
 } as const;
