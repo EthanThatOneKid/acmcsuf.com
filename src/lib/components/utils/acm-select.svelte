1e14586b19ec685d6cc92bba2b8708ee613f11a4
danieltruong <aduy1122@gmail.com>
2021-10-23 16:13:52 -0700

    Modify acm-select.svelte to create a custom select dropdown

--- src/lib/components/utils/acm-select.svelte
+++ src/lib/components/utils/acm-select.svelte
@@ -1,37 +1,89 @@
 <script lang="ts">
-	export let defaultValue: string = '';
+	export let defaultValue: string | HTMLInputElement = '';
 	export let options: string[] = [];
+
+	let option: HTMLDivElement;
+	let dropTitle: HTMLDivElement;
+
+	const setDrop = () => {
+		option.classList.toggle('active');
+	};
+
+	const handleOption = (term) => {
+		dropTitle.innerHTML = term;
+		option.classList.remove('active');
+		defaultValue = term;
+	};
 </script>
 
-<select name="school-year" bind:value={defaultValue}>
-	{#each options as optionValue (optionValue)}
-		<option value={optionValue}>{optionValue}</option>
-	{/each}
-</select>
+<div class="term" name="school-year">
+	<div class="option-box">
+		<div class="selected" bind:this={dropTitle} on:click={setDrop}>{defaultValue}</div>
+		<div class="option active" bind:this={option}>
+			{#each options as optionValue (optionValue)}
+				<div>
+					<input
+						value={optionValue}
+						on:click={() => handleOption(optionValue)}
+						type="radio"
+						class="radio"
+						id={optionValue} />
+					<label for={optionValue}> {optionValue} </label>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
 
-<style>
-	select {
-		text-align: center;
-		font-size: 18px;
+<style lang="scss">
+	.term {
 		font-weight: 600;
-		padding: 8px 20px;
-		border: none;
-		border-radius: 8px;
-		background-color: var(--acm-dark);
-		color: var(--acm-light);
-		outline: none;
-		appearance: none;
-		transition: background-color 0.25s ease-in-out;
+		.option-box {
+			flex-direction: column;
+			display: flex;
+			justify-content: center;
+		}
 	}
 
-	select:hover {
+	label,
+	.selected {
+		color: white;
+	}
+	.selected {
+		background-color: var(--acm-dark);
+		padding: 8px 24px;
 		cursor: pointer;
-		background-color: var(--acm-blue);
+		border-radius: 6px 6px 0px 0px;
+		&:hover {
+			color: var(--acm-blue);
+		}
 	}
 
-	option {
+	.option {
+		cursor: pointer;
+		visibility: hidden;
+		margin-bottom: 0.5rem;
+		transition: all 300ms;
+		label {
+			cursor: pointer;
+			&:hover {
+				color: transparent;
+			}
+		}
+	}
+	.radio {
+		display: none;
+	}
+
+	.option.active {
+		visibility: visible;
 		background-color: var(--acm-dark);
-		color: var(--acm-light);
-		font-weight: 500;
+		padding: 8px 24px;
+		margin-top: 0.2rem;
+		border-radius: 0 0 6px 6px;
+		transition: all 200ms;
+		label:hover {
+			color: var(--acm-blue);
+		}
 	}
 </style>
