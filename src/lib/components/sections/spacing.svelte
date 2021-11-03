EthanThatOneKid/acmcsuf.com

commit 8c0c4f3743c473649c8fd3e39048b90ac4e19516

Refactored Spacing component to use CSS variables (#184)
* Refactored Spacing component to use CSS variables according to https://svelte.dev/docs#style_props>.
* Shortened the property names into CSS variable names.

src/lib/components/sections/spacing.svelte
@@ -1,32 +1,22 @@
-<script lang="ts">
-	import { styleProps } from '$lib/actions/use-style-props';
-	export let amount: string = '100px';
-	export let minAmount: string = amount;
-	export let maxAmount: string = amount;
-</script>
-
-<div
-	use:styleProps={{
-		'min-spacing-height': minAmount,
-		'med-spacing-height': amount,
-		'max-spacing-height': maxAmount
-	}}
-/>
+<div />
 
 <style>
 	div {
-		height: var(--min-spacing-height);
+		--med: 100px;
+		--min: var(--med, 100px);
+		--max: var(--med, 100px);
+		height: var(--min);
 	}
 
 	@media screen and (min-width: 768px) {
 		div {
-			height: var(--med-spacing-height);
+			height: var(--med);
 		}
 	}
 
 	@media screen and (min-width: 1440px) {
 		div {
-			height: var(--max-spacing-height);
+			height: var(--max);
 		}
 	}
 </style>

src/lib/components/utils/skip-navbar-content.svelte
@@ -2,4 +2,4 @@
 	import Spacing from '$lib/components/sections/spacing.svelte';
 </script>
 
-<Spacing amount="100px" minAmount="200px" maxAmount="250px" />
+<Spacing --med="100px" --min="200px" --max="250px" />

src/routes/__layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Navbar from '$lib/components/sections/navbar.svelte';
+	import SkipNavbarContent from '$lib/components/utils/skip-navbar-content.svelte';
 	import Footer from '$lib/components/sections/footer.svelte';
 
 	export let segment: string;
 </script>
 
 <Navbar {segment} />
+<SkipNavbarContent />
 <main><slot /></main>
 <Footer />
 

src/routes/about.svelte
@@ -14,11 +14,11 @@
 	};
 </script>
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <Header />
 
-<Spacing minAmount="32px" maxAmount="64px" />
+<Spacing --min="32px" --max="64px" />
 
 <CommonHero>
 	<h2 slot="headline" class="size-l">Who are we?</h2>
@@ -36,14 +36,13 @@
 	</p>
 </CommonHero>
 
-<Spacing amount="150px" />
+<Spacing --med="150px" />
 
 <!-- TODO: Get the content below from the `/events.json` endpoint and just
 display the most upcoming event with a reminder/notice flair. -->
 <Admonition
 	path="with-shadow/nodebuds"
-	expiration={new Date('Sat Oct 07 2021 00:00:00 GMT-0700').valueOf()}
->
+	expiration={new Date('Sat Oct 07 2021 00:00:00 GMT-0700').valueOf()}>
 	Looking to improve your computer science skills AND make a friend in the process? Join <span
 		class="headers brand-light">node<span class="brand-em brand-red">Buds</span></span
 	>, our mentorship program where students get paired with
@@ -53,17 +52,17 @@ display the most upcoming event with a reminder/notice flair. -->
 	<a href="/nbapp" class="link headers brand-light">Click here to apply now!</a>
 </Admonition>
 
-<Spacing amount="150px" />
+<Spacing --med="150px" />
 
 <div class="container">
 	<h2 class="headers size-l">Board members</h2>
 </div>
 
-<Spacing amount="16px" />
+<Spacing --med="16px" />
 
 <OfficerProfileList filter={filterOfficers} placeholderPicture="placeholder.png" />
 
-<Spacing minAmount="40px" amount="95px" maxAmount="120px" />
+<Spacing --min="40px" --med="95px" --max="120px" />
 
 <style lang="scss">
 	@import 'static/theme.scss';

src/routes/connect.svelte
@@ -7,7 +7,7 @@
 	const googleFormId = '1FAIpQLSfJanOAaL2mdjpf193tFeCClBzpW_COEO_crAE8hqsJCB_Rwg';
 </script>
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <CommonHero>
 	<h2 slot="headline" class="size-l">Connect with us!</h2>
@@ -18,15 +18,15 @@
 	</p>
 </CommonHero>
 
-<Spacing amount="64px" />
+<Spacing --med="64px" />
 
 <SocialMediaLinks />
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <GetInTouchForm {googleFormId} />
 
-<Spacing minAmount="40px" amount="95px" maxAmount="120px" />
+<Spacing --min="40px" --med="95px" --max="120px" />
 
 <style lang="scss">
 	@import 'static/theme.scss';

src/routes/events.svelte
@@ -20,7 +20,7 @@
 	});
 </script>
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <CommonHero>
 	<h2 slot="headline" class="size-l">Curated events for growth and success</h2>
@@ -29,16 +29,15 @@
 		workshops, info sessions, community building events, and much more!
 		<br /><br />
 		<span class="brand-med"
-			>Events are open to anyone interested, regardless of major or background experience.</span
-		>
+			>Events are open to anyone interested, regardless of major or background experience.</span>
 	</p>
 </CommonHero>
 
-<Spacing minAmount="100px" amount="125px" maxAmount="125px" />
+<Spacing --min="100px" --med="125px" --max="125px" />
 
 <h2 class="size-l headers">This week's events 📅</h2>
 
-<Spacing amount="16px" />
+<Spacing --med="16px" />
 
 {#if events.length > 0}
 	<EventCarousel {events} />
@@ -54,7 +53,7 @@
 	</AcmEmpty>
 {/if}
 
-<Spacing minAmount="8px" amount="63px" maxAmount="88px" />
+<Spacing --min="8px" --med="63px" --max="88px" />
 
 <style lang="scss">
 	@import 'static/theme.scss';

src/routes/index.svelte
@@ -6,20 +6,20 @@
 	import ReadyUp from '$lib/components/index/ready-up.svelte';
 </script>
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <Hero />
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <WhyJoin />
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <AcmPaths />
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <ReadyUp />
 
-<Spacing minAmount="40px" amount="95px" maxAmount="120px" />
+<Spacing --min="40px" --med="95px" --max="120px" />

src/routes/nodebuds.svelte
@@ -14,7 +14,7 @@
 	};
 </script>
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <div class="container">
 	<section>
@@ -26,8 +26,7 @@
 				</span>
 				is our exclusive program in partnership with
 				<a href="/acm-w" class="w-text" target="_blank" rel="noopener noreferrer">
-					<span class="headers">ACM </span>W</a
-				>
+					<span class="headers">ACM </span>W</a>
 				that exposes students to various opportunities that encourage connection, skill building, as
 				well as both personal and technical development.
 				<br /><br />
@@ -65,17 +64,17 @@
   </p>
 </NodeBudsTestimonial> -->
 
-<Spacing minAmount="100px" amount="175px" maxAmount="200px" />
+<Spacing --min="100px" --med="175px" --max="200px" />
 
 <div class="container">
 	<h2 class="headers size-l">Buddies</h2>
 </div>
 
-<Spacing amount="16px" />
+<Spacing --med="16px" />
 
 <OfficerProfileList filter={filterNodeBuddies} />
 
-<Spacing minAmount="40px" amount="95px" maxAmount="120px" />
+<Spacing --min="40px" --med="95px" --max="120px" />
 
 <style lang="scss">
 	@import 'static/theme.scss';

commit bcae31e81299afe3e02e13a2e407a7338867eefb

Fix Spacing component parameters
This commit fixes the Spacing component's CSS variables being
incorrectly set. They didn't work prior to this change, because of a
regression from PR #184.

src/lib/components/sections/spacing.svelte
@@ -1,10 +1,13 @@
 <div />
 
 <style>
-	div {
+	:root {
 		--med: 100px;
 		--min: var(--med, 100px);
 		--max: var(--med, 100px);
+	}
+
+	div {
 		height: var(--min);
 	}
 