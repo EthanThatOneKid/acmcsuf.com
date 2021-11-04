ca2ba0db4830e8182ebb3d5c7f59b643f96c1047
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-11-03 21:25:34 -0700

    Added ARCHIVE_CHANNEL_ID environment variable

--- scripts/close-issue-channel/common.js
+++ scripts/close-issue-channel/common.js
@@ -1,32 +1,18 @@
-const findFirstChannelInCategory = (channels, categoryId) => {
-	let firstChannel = null;
-	for (const channel of channels) {
-		if (
-			channel.parentId === categoryId &&
-			(firstChannel === null || channel.position < firstChannel.position)
-		) {
-			firstChannel = channel;
-		}
-	}
-	return firstChannel;
-};
-
 export const closeIssueChannel = async (client, issueNumber) => {
 	let success = false;
 	try {
 		await client.guilds.fetch();
 		const { channels } = client.guilds.cache.get(process.env.GUILD_ID);
-		const hubChannel = findFirstChannelInCategory(channels, process.env.HUB_ID);
-		if (hubChannel === null) {
-			console.log(`No hub channel found.`);
+		const archiveChannel = channels.find((ch) => ch.id === process.env.ARCHIVE_CHANNEL_ID);
+		if (archiveChannel === null) {
+			console.log(`No archive channel found.`);
 			return false;
 		}
-		for (const channel of channels.cache) {
+		for (const channel of channels) {
 			// Only close the channel if it matches the issue number.
 			if (channel.parentId !== process.env.HUB_ID) continue;
 			if (!channel.name.startsWith(`website-issue-${issueNumber}`)) continue;
-			// TODO: Check if hub channel is always at position 0.
-			console.log({ hubChannel, closedChannel, channels: [...channels.cache] });
+			console.log('Channel: ', channel);
 			// TODO: Fetch all messages from `closedChannel` text channel.
 			// TODO: Print messages out to TXT file format.
 			// TODO: Send TXT file to `hubChannel` text channel.
