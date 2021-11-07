e7fb15a5fed7df5a64a98ffd51b7c2100fdc28f2
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-11-06 18:34:32 -0700

    Eliminated reliance on entire issue object for create-issue-channel script

--- scripts/create-issue-channel.js
+++ scripts/create-issue-channel.js
@@ -2,25 +2,26 @@ import { startBot, getNArg } from './common.js';
 
 startBot(async (client) => {
   console.log(`Logged in as ${client.user.tag}`);
-  const issue = getNArg(2);
-  const success = await createIssueChannel(client, issue);
+  const issueNumber = getNArg(2);
+  const success = await createIssueChannel(client, issueNumber);
   client.destroy();
   console.log(`Success: ${success}`);
   process.exit(success ? 0 : 1);
 });
 
-const createIssueChannel = async (client, issue) => {
+const createIssueChannel = async (client, issueNumber) => {
   let success = false;
   try {
-    const channelName = `website-issue-${issue.number}`;
+    const channelName = `website-issue-${issueNumber}`;
     await client.guilds.fetch();
     const { channels } = client.guilds.cache.get(process.env.GUILD_ID);
     const channel = await channels.create(channelName, {
       type: 'GUILD_TEXT',
       parent: process.env.HUB_ID,
-      reason: `Let's resolve #${issue.number}!`,
+      reason: `Let's resolve #${issueNumber}!`,
     });
-    const firstMessage = await channel.send(issue.html_url);
+    const baseUrl = 'https://github.com/EthanThatOneKid/acmcsuf.com/issues/';
+    const firstMessage = await channel.send(baseUrl + issueNumber);
     await firstMessage.pin();
     success = true;
   } catch (error) {
