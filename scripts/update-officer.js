880afbb1a9c93234ad63b605dc69d74c4997eac1
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-11-08 21:15:42 -0800

    Updated officer update request form

--- scripts/update-officer.js
+++ scripts/update-officer.js
@@ -1,9 +1,51 @@
 import { getNArg } from './common.js';
 
 const updateOfficer = async () => {
-  const payload = getNArg(2);
-  console.log('PAYLOAD: ', { payload });
-  // TODO: Use the data to update the officer data locally.
+  const result = JSON.parse(readFileSync(OFFICERS_FILENAME));
+  const report = [];
+  const {
+    ['Officer Name']: name,
+    ['Term to Overwrite']: term,
+    ['Overwrite Officer Position Title']: title,
+    ['Overwrite Officer Picture']: picture,
+  } = JSON.parse(process.env.FORM_DATA);
+  const isValidName = name?.trim().length > 0 ?? false;
+  if (!isValidName) {
+    console.error(`received invalid officer name ${name}`);
+    return false;
+  }
+  const officerIndex = result.findIndex((officer) => officer.name === name);
+  const abbreviatedTerm = termAbbr(term);
+  if (abbreviatedTerm === null) {
+    console.error(`received invalid term, '${term}'`);
+    return false;
+  }
+  if (officerIndex === -1) {
+    // officer name not found, so let's create a new officer
+    const positions = { [abbreviatedTerm]: title };
+    result.push({ name, positions, picture });
+    report.push(`Added new officer, ${name}.`);
+  } else {
+    // officer name found, so let's update the officer
+    const titleNeedsUpdate = title !== undefined && title.trim().length > 0;
+    const pictureNeedsUpdate = picture !== undefined && picture.trim().length > 0;
+    if (titleNeedsUpdate) {
+      if (title === 'DELETE') delete result[officerIndex].positions[abbreviatedTerm];
+      else result[officerIndex].positions[abbreviatedTerm] = title.trim();
+    }
+    if (pictureNeedsUpdate) {
+      const imgSrc = parseImgSrcFromMd(picture);
+      if (imgSrc === null) {
+        console.error(`received invalid officer picture '${picture}'`);
+        return false;
+      }
+      const relativeImgSrc = await downloadOfficerImage(imgSrc, name);
+      if (typeof relativeImgSrc === 'string') result[officerIndex].picture = relativeImgSrc;
+    }
+    console.log(`${name.trim()}'s updated officer data: `, result[officerIndex]);
+  }
+  writeFileSync(OFFICERS_FILENAME, JSON.stringify(result, null, 2));
+  return true;
 };
 
 try {
