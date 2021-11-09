2f98b04dba5c5083184ccd57a12d9287e86c454d
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-11-08 22:13:54 -0800

    Revert "Chaining <https://github.com/EthanThatOneKid/acmcsuf.com/commit/7df888109c646cf9eb3127b966245c80bf2c802d>"
    This reverts commit 6cae15f38dee1978ad5612f015e45c56a5a23e3e.

--- scripts/update-officer.js
+++ scripts/update-officer.js
@@ -1,89 +1,9 @@
 import { getNArg } from './common.js';
-import { readFileSync } from 'fs';
-
-/**
- * Example officer data:
- * ```json
- * {
- *	"name": "Ethan Davidson",
- *	"positions": {
- *		"F20": "Competition Manager",
- *		"S21": "Webmaster",
- *		"F21": "Webmaster"
- *	},
- *	"picture": "ethan-davidson.png"
- * }
- *```
- */
-const OFFICERS_FILENAME = './src/lib/constants/officers.json';
-
-/**
- * Converts 'Fall 2021' to 'F21', 'Spring 2022' to 'S22, etc.
- */
-const termAbbr = (term) => (term.startsWith('F') ? 'F' : 'S') + term.slice(term.length - 2);
-
-/**
- * Parses the first instance of an image URL from a markdown string.
- */
-const parseImgSrcFromMd = (markdown) => {
-  const pattern = /!\[[^\]]*\]\((?<filename>.*?)(?=\"|\))(?<optionalpart>\".*\")?\)/i; // https://regex101.com/r/cSbfvF/3/
-  const match = pattern.exec(markdown);
-  if (match !== null) {
-    const {
-      groups: [src],
-    } = match;
-    return src;
-  }
-  return null;
-};
-
-const downloadOfficerImage = async (url, officerName) => {
-  const cleanOfficerName = officerName.trim().toLowerCase().replace(/\s/g, '-');
-  const filename = `./static/assets/authors/${encodeURIComponent(cleanOfficerName)}.png`;
-  const response = axios({ url, responseType: 'stream' });
-  return await new Promise((resolve, reject) => {
-    response.data
-      .pipe(fs.createWriteStream(image_path))
-      .on('finish', () => resolve(filename))
-      .on('error', reject);
-  });
-};
 
 const updateOfficer = async () => {
-  const result = JSON.parse(readFileSync(OFFICERS_FILENAME));
-  const {
-    ['Officer Name']: name,
-    ['Term to Overwrite']: term,
-    ['Overwrite Officer Position Title']: title,
-    ['Overwrite Officer Picture']: picture,
-  } = JSON.parse(getNArg(2));
-  const isValidName = name?.trim().length > 0 ?? false;
-  if (!isValidName) {
-    console.error(`Received invalid officer name ${name}.`);
-    return false;
-  }
-  const officerIndex = result.findIndex(({ name }) => name === officerName);
-  const abbreviatedTerm = termAbbr(term);
-  if (officerIndex === -1) {
-    // officer name not found, so let's create a new officer
-    const positions = { [abbreviatedTerm]: title };
-    result.push({ name, positions, picture });
-  } else {
-    // officer name found, so let's update the officer
-    const titleNeedsUpdate = title !== undefined && title.trim().length > 0;
-    const pictureNeedsUpdate = picture !== undefined && picture.trim().length > 0;
-    if (titleNeedsUpdate) result[officerIndex].positions[abbreviatedTerm] = title;
-    if (pictureNeedsUpdate) {
-      const imgSrc = parseImgSrcFromMd(picture);
-      if (imgSrc === null) {
-        console.error(`Received invalid officer picture ${picture}.`);
-        return false;
-      }
-      const imagePath = await downloadOfficerImage(imgSrc, name);
-      if (typeof imagePath === 'string') result[officerIndex].picture = picture;
-    }
-  }
-  fs.writeFileSync(OFFICERS_FILENAME, JSON.stringify(result, null, 2));
+  const payload = getNArg(2);
+  console.log('PAYLOAD: ', { payload });
+  // TODO: Use the data to update the officer data locally.
 };
 
 try {
