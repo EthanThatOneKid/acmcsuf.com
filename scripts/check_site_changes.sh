2fba6975ec9e48f25d685d7aed23720245cd09ae
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-11-11 17:05:47 -0800

    Replaced check_site_changes.ts with check_site_changes.sh

---
+++ scripts/check_site_changes.sh
@@ -0,0 +1,7 @@
+# Name: check_site_changes.sh
+# Description: Checks for changes in the site and succeeds if there are none.
+# Usage: bash scripts/check_site_changes.sh
+# Tracker: https://etok.codes/acmcsuf.com/issues/198
+# Requires: Git (https://git-scm.com/)
+
+[[ "$(git status --porcelain -- src/ static/ package*.json svelte.config.js)" ]] && exit 1 || exit 0
