56415b23739cd18b2940afb64462ebd5bd265c83
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2021-11-16 17:06:05 -0800

    Update check_site_changes.sh

--- scripts/check_site_changes.sh
+++ scripts/check_site_changes.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Name: check_site_changes.sh
-# Description: Checks for changes in the site and succeeds if there are none.
+# Description: Exits with code 1 (new build needed) or code 0
 # Usage: bash scripts/check_site_changes.sh
 # Tracker: https://etok.codes/acmcsuf.com/issues/198
 # Requires: Git (https://git-scm.com/)
 
-[[ "$(git status --porcelain -- src/ static/ package*.json svelte.config.js)" ]] && exit 1 || exit 0
+[[ "$(git status --porcelain -- src/ static/ package*.json svelte.config.js)" ]] && exit 1
