82e5b0aa39833f38aff9902e9444fb78d502edff
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-11-26 19:28:18 -0800

    progress from work sesh e

---
+++ src/routes/newsletters/[id].svelte
@@ -0,0 +1,18 @@
+<script lang="ts" context="module">
+  /**
+   * @type {import('@sveltejs/kit').Load}
+   */
+  export async function load({ fetch, page }) {
+    const id = page.params.id;
+    return { props: { posts: [], id } };
+  }
+</script>
+
+<script lang="ts">
+  export let posts: any[];
+  export let id: string;
+</script>
+
+<h1>Newsletter!</h1>
+
+<p>{id}</p>
---
+++ src/routes/newsletters/index.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<h1>Hello Newsletter</h1>
+
+TODO: Code a list of newsletter post preview cards
