9a43fc5a61787238ac597c3fa913f136e4024566
mikeploythai <54593375+mikeploythai@users.noreply.github.com>
2022-01-23 18:12:21 -0800

    Implemented 404 page graphics

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,53 +1,51 @@
 <script>
   import { dev } from '$app/env';
   export let status = 404;
-  export let error = new Error('Page Not Found');
+  /* export let error = new Error('Page Not Found'); */
 </script>
 
 <svelte:head>
-  <title>{status}</title>
+  <title>acmCSUF / {status}</title>
 </svelte:head>
 
-<h1>{status} {error.message}</h1>
+<section>
+  <img src="assets/png/404-h.png" class="wide" alt="404 - Page Not Found" />
+  <img src="assets/png/404-v.png" class="mobile" alt="404 - Page Not Found" />
+</section>
+
+<!-- <h1>{status} {error.message}</h1>
 <div class="image-container">
   <img src="/assets/png/acm-shark.png" alt="ACM mascot with 404 error" />
 </div>
 {#if dev && error.stack}
   <pre><code>{error.stack}</code></pre>
-{/if}
-
+{/if} -->
 <style>
-  h1 {
-    font-size: 2.8em;
-    font-weight: 700;
-    padding-top: 1.5em;
-    margin: 0 auto;
-  }
-
-  .image-container {
-    width: 100%;
+  section {
+    min-height: 100vh;
     display: flex;
     justify-content: center;
-    padding: 4em 0 4em 0;
+    align-items: center;
+    margin: 0 24px;
   }
 
-  img {
-    width: 200px;
+  section .mobile {
+    display: none;
   }
 
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
+  section img {
+    width: 100%;
+    max-width: 1000px;
+    height: auto;
+  }
+
+  @media (max-width: 900px) {
+    section .wide {
+      display: none;
     }
 
-    img {
-      width: 400px;
-    }
-  }
-
-  @media (min-width: 770px) {
-    img {
-      width: 600px;
+    section .mobile {
+      display: unset;
     }
   }
 </style>
