a6482b50ee54543d78d2e228790260d4e2f364e8
diamondburned <diamond@arikawa-hi.me>
2022-02-03 15:00:43 -0800

    Add description into EventItem

--- src/lib/components/events/event-item.svelte
+++ src/lib/components/events/event-item.svelte
@@ -5,44 +5,53 @@
   export let info: AcmEvent;
 
   let isRecurring: boolean = info.recurring;
+  let anchor: HTMLElement;
+  let details: HTMLDetailsElement;
 
   onMount(() => {
     if (location.hash === `#${info.slug}`) {
-      document.getElementById(info.slug).scrollIntoView({
+      anchor.scrollIntoView({
         behavior: 'smooth',
         block: 'start',
         inline: 'center',
       });
+      // This has the same styling as :target, and :target doesn't want to work
+      // for dynamically-added elements.
+      details.open = true;
     }
   });
 </script>
 
 <div class="event-box">
   <!-- Workaround for the top panel covering the event card's anchor. -->
-  <div class="anchor" id={info.slug} />
-  <div class="event-card">
-    <div class="event-name">
-      <h2 class="headers">
-        <a href="#{info.slug}">
-          {info.summary}
-        </a>
-      </h2>
-      <p class="event-location">
-        {info.location === 'Discord' || info.location === 'Zoom'
-          ? `Hosted on ${info.location}`
-          : info.location}
+  <div class="anchor" id={info.slug} bind:this={anchor} />
+  <details class="event-card" bind:this={details}>
+    <summary class="event-body">
+      <div class="event-name">
+        <h2 class="headers">
+          <a href="#{info.slug}">
+            {info.summary}
+          </a>
+        </h2>
+        <p class="event-location">
+          {info.location === 'Discord' || info.location === 'Zoom'
+            ? `Hosted on ${info.location}`
+            : info.location}
+        </p>
+      </div>
+      <p class="event-date">
+        <!-- TODO: RFC3339 timestamp for datetime -->
+        <time>
+          {info.month}
+          {info.day} at {info.time}
+          {#if isRecurring}(recurring){/if}
+        </time>
       </p>
-    </div>
-    <p class="event-date">
-      <!-- TODO: RFC3339 timestamp for datetime -->
-      <time>
-        {info.month}
-        {info.day} at {info.time}
-        {#if isRecurring}(recurring){/if}
-      </time>
-    </p>
-    <a class="event-join size-s" href={info.meetingLink} role="button">Join</a>
-  </div>
+      <a class="event-join size-s" href={info.meetingLink} role="button">Join</a>
+    </summary>
+    <hr />
+    <p class="event-description">{info.description}</p>
+  </details>
 </div>
 
 <style lang="scss">
@@ -60,16 +69,21 @@
   }
 
   .event-card {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
     margin: 32px 64px;
-    padding: 24px 30px;
+    padding: 0;
     box-shadow: 0 6px 18px rgba(44, 145, 198, 0.25);
-    transition: all 0.25s ease-in-out;
+    transition: all 0.15s ease-in-out;
     border-radius: 30px;
     border: 2px solid var(--acm-dark);
+  }
+
+  .event-card:hover {
+    box-shadow: 0 6px 18px rgba(44, 145, 198, 0.65);
+  }
+
+  .event-card[open] {
+    box-shadow: 0 6px 24px rgba(44, 145, 198, 0.75);
+    border: 2px solid var(--acm-blue);
   }
 
   .event-box > .anchor:target + .event-card {
@@ -77,16 +91,44 @@
     border: 2px solid var(--acm-blue);
   }
 
-  .event-card a {
+  .event-card > hr {
+    border-width: 1px;
+    border-color: var(--acm-dark);
+    opacity: 0.5;
+    margin: 24px 0;
+  }
+
+  .event-card > hr,
+  .event-description {
+    margin: 0 30px;
+  }
+
+  .event-body {
+    /* Do this whole dance so the user can click on the padding. */
+    padding: 24px 30px;
+    cursor: pointer;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  /* Safari workaround for list-style: none. */
+  summary.event-body::-webkit-details-marker {
+    display: none;
+  }
+
+  .event-body a {
     text-decoration: none;
     transition: all 0.15s ease-in-out;
   }
 
-  .event-card a:hover {
+  .event-body a:hover {
     color: var(--acm-blue);
   }
 
-  .event-card h2 {
+  .event-body h2 {
     position: relative;
     user-select: none;
     color: var(--acm-dark);
@@ -137,8 +179,19 @@
     transition: background-color 0.25s ease-in-out;
   }
 
+  .event-description {
+    margin-top: 24px;
+    margin-bottom: 24px;
+  }
+
+  .event-description:empty::after {
+    content: 'No description.';
+    opacity: 0.75;
+    font-style: italic;
+  }
+
   @media (max-width: 799px) {
-    .event-card {
+    .event-body {
       flex-direction: column;
     }
     .event-name {
