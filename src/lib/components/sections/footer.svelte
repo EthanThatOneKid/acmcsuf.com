c2ba13dbcfc6ab05594002e76cd1ce02e50ad0f3
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-02-11 02:55:14 -0800

    done

--- src/lib/components/sections/footer.svelte
+++ src/lib/components/sections/footer.svelte
@@ -47,9 +47,7 @@
         </h1>
 
         <div class="links brand-light">
-          <a href="/privacy" class="brand-light" rel="noopener noreferrer">
-            frankBot Privacy
-          </a>
+          <a href="/privacy" class="brand-light" rel="noopener noreferrer"> frankBot Privacy </a>
 
           /
 
