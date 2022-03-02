97a14f496a706937ee8fb1f9e460779667bd6338
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-03-02 08:57:29 -0800

    Made toast animation one dimensional

--- src/lib/components/utils/acm-toaster.svelte
+++ src/lib/components/utils/acm-toaster.svelte
@@ -7,8 +7,8 @@
   {#each $toasts as toastItem (toastItem.id)}
     <div
       class="toast-item"
-      in:fly={{ x: 20, y: 20 }}
-      out:fly={{ x: 20, y: -20 }}
+      in:fly={{ y: 20 }}
+      out:fly={{ y: -20 }}
       style={`--highlights: var(--${toastItem.type}-rgb)`}>
       {@html toastItem.content}
     </div>
@@ -27,13 +27,12 @@
     z-index: 10000;
     bottom: 0;
     right: 0;
-    visibility: var(--visibility, inherit);
   }
 
   .toast-item {
     margin: 32px 32px;
     padding: 2em;
-    box-shadow: 0 6px 18px rgba(var(--highlights, --general-rgb), 0.25);
+    box-shadow: 0 6px 18px rgba(var(--highlights, --info-rgb), 0.25);
     transition: all 0.25s ease-in-out;
     border-radius: 30px;
     border: 2px solid var(--acm-dark);
