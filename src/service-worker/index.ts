99885972755c34e9686a4cfc606d80684a771b98
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-03-04 13:11:32 -0800

    try this

--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,34 +1,4 @@
 /// <reference lib="webworker" />
-import { CURRENT_CACHE_NAME, BUILD_FILES, CACHE_BLOCKLIST } from './common';
-import { fromNetwork } from './from/network';
-import { fromCache } from './from/cache';
-
-self.addEventListener('activate', (event: ExtendableEvent) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        // Delete old caches with an expired cache name
-        cacheNames.map(
-          (cacheName) =>
-            cacheName !== CURRENT_CACHE_NAME &&
-            !CACHE_BLOCKLIST.has('/' + cacheName) &&
-            caches.delete(cacheName)
-        )
-      )
-    )
-  );
-});
-
-self.addEventListener('install', (event: ExtendableEvent) => {
-  event.waitUntil(
-    // Cache known build output and static files
-    // @see <https://kit.svelte.dev/docs/modules#$service-worker-build>
-    caches.open(CURRENT_CACHE_NAME).then((cache) => cache.addAll(BUILD_FILES))
-  );
-});
-
-self.addEventListener('fetch', (event: FetchEvent) => {
-  // If online, try to fetch from network with a timeout.
-  // If anything fails, try to serve from cache.
-  event.respondWith(fromNetwork(event.request, 10e3).catch(() => fromCache(event.request)));
+self.addEventListener('install', () => {
+  console.log('Thank you for installing.');
 });
