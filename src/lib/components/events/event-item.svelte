4a247b63a95730ca8733adcf466248e2ea56cea5
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-03-17 13:42:37 -0700

    Found StackOverflow response to help with share jank

--- src/lib/components/events/event-item.svelte
+++ src/lib/components/events/event-item.svelte
@@ -23,10 +23,37 @@
       .catch(() => toast({ path, type: ToastType.Error, content: errorMessage }));
   }
 
+  /** @see <https://stackoverflow.com/a/57711953> */
   function share(info: AcmEvent) {
-    navigator.share({
-      title: info.title,
-      url: info.selfLink,
+    return new Promise((resolve, reject) => {
+      navigator
+        .share({ title: info.title, url: info.selfLink })
+        .then(resolve)
+        .catch((error) => {
+          // Differentiate between user 'AbortError' and internal errors.
+          // E.g. Internal error: could not connect to Web Share interface.
+          if (error.message.startsWith('Internal error:')) error.name = 'InternalError';
+
+          toast({ type: ToastType.Error, content: 'Internal error.', path: info.acmPath.slug });
+          reject(error);
+        });
+
+      /*
+       * https://bugs.chromium.org/p/chromium/issues/detail?id=636274
+       * If the share promise is not resolved or rejected when focus is returned to
+       * the window, then reject it after a 100ms delay.
+       */
+      function cancel() {
+        return setTimeout(() => {
+          removeEventListener('focus', cancel);
+
+          const error = new Error('Share cancelled');
+          error.name = 'ShareTimeout';
+          reject(error);
+        }, 1e2);
+      }
+
+      addEventListener('focus', cancel);
     });
   }
 
