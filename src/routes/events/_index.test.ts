ac8431b9bec99346391e2402e56dbc56026cc6dd
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-03-18 14:37:45 -0700

    Added tests and testdata for #329
    Sorry, @Angel-Armendariz, I know you just reviewed it! 😅

--- src/routes/events/_index.test.ts
+++ src/routes/events/_index.test.ts
@@ -1,7 +1,9 @@
-import { beforeEach, test, expect } from 'vitest';
+import { readFileSync } from 'fs';
+import { beforeEach, expect, test } from 'vitest';
 import { cleanup, render } from '@testing-library/svelte';
 
 import Events from './index.svelte';
+import { parse } from '$lib/ical/parse';
 
 beforeEach(cleanup);
 
@@ -12,4 +14,11 @@ test('can render', () => {
 test('can find the correct page title', () => {
   const { getByText } = render(Events);
   expect(getByText('Curated events for growth and success')).toBeDefined();
+});
+
+test('renders 10 event items', async () => {
+  const TEST_DATA = readFileSync('./src/routes/events/_testdata/events.ics', 'utf-8');
+  const TEST_EVENTS = parse(TEST_DATA);
+  const { container } = render(Events, { events: TEST_EVENTS });
+  expect(container.querySelectorAll('.event-box').length).toBe(10);
 });
