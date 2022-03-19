7d587f873b710c1b591726f80ae92543b8ec7e46
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-03-18 18:07:50 -0700

    Ran `npm run lint`
    Co-Authored-By: Angel Armendariz <95111582+Angel-Armendariz@users.noreply.github.com>
    Co-Authored-By: Dylan Morris <dylmorris16@gmail.com>
    Co-Authored-By: Angel  <26943671+Angus-1@users.noreply.github.com>
    Co-Authored-By: Joel Anil John <56321852+janiljohn@users.noreply.github.com>

--- src/lib/components/utils/acm-toggle.svelte
+++ src/lib/components/utils/acm-toggle.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
 
-  export let initialValue: boolean = false;
+  export let initialValue = false;
 
   let value = initialValue;
 
