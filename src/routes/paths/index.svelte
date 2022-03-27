99b0589c7bf8f7f9663ab0d2f4ce8ba7e74a9e62
mikeploythai <54593375+mikeploythai@users.noreply.github.com>
2022-03-26 19:10:04 -0700

    Corrected text sizing for paragraphs - Also deprecated old connect page

--- src/routes/paths/index.svelte
+++ src/routes/paths/index.svelte
@@ -10,7 +10,7 @@
 
 <CommonHero>
   <h2 slot="headline" class="size-lg">What are paths?</h2>
-  <p slot="text" class="size-xs">
+  <p slot="text" class="size-sm">
     Paths are committees that specialize in specific fields in the tech industry. We’ve designed
     paths to be gateways for students to explore new fields, develop new interests, and enhance
     skills that’ll benefit in the industry.
@@ -20,7 +20,7 @@
 <Spacing --min="100px" --med="175px" --max="200px" />
 
 <PathSection info={acmAlgo} textAlign={TextAlignment.Right}>
-  <p slot="content" class="size-xs">
+  <p slot="content" class="size-sm">
     This path is dedicated to building the programming proficiency of students.
     <span class="brand-purple brand-em">Algo</span> focuses on mastering data structures and algorithms,
     enhancing problem solving abilities, and exploration of competitive programming.
@@ -30,7 +30,7 @@
 <Spacing --med="64px" />
 
 <PathSection info={acmCreate} textAlign={TextAlignment.Left}>
-  <p slot="content" class="size-xs">
+  <p slot="content" class="size-sm">
     This path is dedicated to emphasizing the importance of product design and product management in
     the tech industry. <span class="brand-pink brand-em">Create</span> focuses on educating students
     about design principles, design tools, and the intricacies of conceptualization, development, and
@@ -41,7 +41,7 @@
 <Spacing --med="64px" />
 
 <PathSection info={acmDev} textAlign={TextAlignment.Right}>
-  <p slot="content" class="size-xs">
+  <p slot="content" class="size-sm">
     This path is dedicated to giving students the opportunity to explore tech via hands-on projects
     and activities. <span class="brand-em brand-bluer">Dev</span> focuses on introducing students to
     software development, and the various tech stacks used in the industry.
