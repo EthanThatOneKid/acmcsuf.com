b4bac680c239ac405d04ef24d823c1bca58216d9
jason wong <jayywong92@gmail.com>
2022-04-03 11:02:48 -0700

    Revert "Minor design tweaks" (#412)
    * Revert "Minor design tweaks"
    
    * Refactored CSS
    - Responsive text sizing via clamp()
    - Transitioning CSS macros to new naming scheme
    - Introduced --perma-light, --card-bg, --button-bg, and --button-hover variables for dynamic theming
    
    * Tweaked toast styling
    
    Co-authored-by: mikeploythai <54593375+mikeploythai@users.noreply.github.com>

--- src/lib/components/about/officer-profile.svelte
+++ src/lib/components/about/officer-profile.svelte
@@ -11,10 +11,13 @@
   const officerPicture = info.picture ?? placeholderPicture;
 
   $: titleHTML = info.positions[VISIBLE_TERMS[$termIndex]].title
-    .replace(/Algo\s/, `<span class="brand-bold acm-purple">Algo&nbsp</span>`)
-    .replace(/Create\s/, `<span class="brand-bold acm-pink">Create&nbsp</span>`)
-    .replace(/Dev\s/, `<span class="brand-bold acm-bluer">Dev&nbsp</span>`)
-    .replace(/nodebuds\s/, `<span class="brand-header">Node Buds&nbsp</span>`);
+    .replace(/Create\s/, `<span class="brand-em brand-pink">Create </span>`)
+    .replace(/Algo\s/, `<span class="brand-em brand-purple">Algo </span>`)
+    .replace(/Dev\s/, `<span class="brand-em brand-bluer">Dev </span>`)
+    .replace(
+      /NodeBuds/,
+      `<span class="headers">node<span class="brand-em brand-red">Buds</span></span>`
+    );
 
   $: officerTier = dev ? TIERS[info.positions[VISIBLE_TERMS[$termIndex]].tier] : '';
 </script>
@@ -25,17 +28,13 @@
     src={`../assets/authors/${officerPicture}`}
     alt={`Image of ${officerName}.`}
   />
-
-  <div>
-    <h3 class="brand-header">
-      {officerName}
-      {#if officerTier.length}<br />{officerTier}<br />{/if}
-    </h3>
-
-    <p class="brand-med">
-      {@html titleHTML}
-    </p>
-  </div>
+  <h3 class="headers">
+    {officerName}
+    {#if officerTier.length}<br />{officerTier}<br />{/if}
+  </h3>
+  <p>
+    {@html titleHTML}
+  </p>
 </div>
 
 <style lang="scss">
@@ -44,15 +43,22 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    padding: 64px 64px 0;
     text-align: center;
+  }
 
-    .officer-image {
-      width: 200px;
-      height: auto;
-    }
+  .officer-image {
+    width: 200px;
+    height: 200px;
+  }
 
-    p {
-      max-width: 250px;
-    }
+  h3 {
+    color: var(--acm-dark);
+  }
+
+  p {
+    font-weight: 500;
+    max-width: 250px;
+    color: var(--acm-dark);
   }
 </style>
