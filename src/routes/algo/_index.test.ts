f84a794e1efc6c97512de5403dd50b2ef4bc3171
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-04-11 09:11:45 -0700

    Update _index.test.ts

--- src/routes/algo/_index.test.ts
+++ src/routes/algo/_index.test.ts
@@ -1,15 +1,15 @@
 import { beforeEach, test, expect } from 'vitest';
 import { cleanup, render } from '@testing-library/svelte';
 
-import Blog from './index.svelte';
+import Algo from './index.svelte';
 
 beforeEach(cleanup);
 
 test('can render', () => {
-  render(Blog);
+  render(Algo);
 });
 
 test('can find the correct page title', () => {
-  const { getByText } = render(Blog);
+  const { getByText } = render(Algo);
   expect(getByText('What is acmAlgo?')).toBeDefined();
 });
