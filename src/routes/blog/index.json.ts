df2b11b6b21be0be11826acc483d3b097259dad5
Mohamed Habarneh <92408806+MohamedHabarneh@users.noreply.github.com>
2022-04-24 12:16:42 -0700

    Update index.json.ts

--- src/routes/blog/index.json.ts
+++ src/routes/blog/index.json.ts
@@ -3,17 +3,16 @@ import type { RequestHandlerOutput } from '@sveltejs/kit/types/internal';
 import { fetchNewsletters } from './_query';
 import { DEBUG } from '$lib/constants';
 
-const SAMPLE_POST = readFileSync('./src/routes/blog/_testdata/posts.json');
+const SAMPLE_POSTS = readFileSync('./src/routes/blog/_testdata/posts.json');
 
 export async function get(): Promise<RequestHandlerOutput> {
-  // Uses sample data when env variables are not satisfied.
+  // Uses sample data when DEBUG = 1 or env variables are not satisfied.
   const isSatisfied =
-    import.meta.env.VITE_GH_ACCESS_TOKEN === undefined ||
-    import.meta.env.VITE_GH_DISCUSSION_CATEGORY_ID === undefined;
+    import.meta.env.VITE_GH_ACCESS_TOKEN !== undefined ||
+    import.meta.env.VITE_GH_DISCUSSION_CATEGORY_ID !== undefined;
 
-  // remove the ! infront of the DEBUG to show only the sample posts
   return new Response(
-    !DEBUG || isSatisfied ? SAMPLE_POST : JSON.stringify(await fetchNewsletters()),
+    DEBUG || !isSatisfied ? SAMPLE_POSTS : JSON.stringify(await fetchNewsletters()),
     {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
