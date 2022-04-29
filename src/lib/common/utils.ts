030f1b2e19153d964ac467fc6bd308767966960a
mle225 <mle225@csu.fullerton.edu>
2022-04-30 03:07:34 +0700

    Update src/lib/common/utils.ts
    Co-authored-by: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>

--- src/lib/common/utils.ts
+++ src/lib/common/utils.ts
@@ -1,5 +1,5 @@
 export function isStringTruthy(payload?: string | null, defaultValue = false): boolean {
   if (!payload) return false;
 
-  return !['false', '0', ''].includes(str.toLowerCase());
+  return !['0', 'false'].includes(payload.trim().toLowerCase());
 }
