7361b8f471532b74970f986af22e8c5b80728d51
jaasonw <jayywong92@gmail.com>
2022-05-25 18:18:21 -0700

    Update parameter name and jsdoc

--- src/lib/blog/util.ts
+++ src/lib/blog/util.ts
@@ -1,13 +1,14 @@
 /**
- * Takes a string (either html or plaintext) and calculates the time
- * (in minutes) to read a blog post, By default it uses 225 the default adult
- * average reading time based on a quick google search, but may be overrided
- * with the wpm parameter
- * @param string The string containing the blog text
+ * Takes a string input (either html or plaintext) and calculates the time
+ * (in minutes) to read a blog post, By default it uses 225 (the default adult
+ * average reading time based on a quick Google search), but may be overridden
+ * with the `wpm` parameter.
+ * @param blogContent The string containing the blog text
  * @param wpm (Optional) defines words per minute to assume the reader reads at
  * @returns The estimated read time of an article in minutes
+ * @see <https://stackoverflow.com/a/5002161>
  */
-export function readingTime(string: string, wpm = 225) {
-  const text = string.replace(/<\/?[^>]+(>|$)/, '').trim();
+export function readingTime(blogContent: string, wpm = 225) {
+  const text = blogContent.replace(/<\/?[^>]+(>|$)/, '').trim();
   return Math.ceil(text.split(/\s+/).length / wpm);
 }
