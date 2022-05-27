548a402630328e58cff78ff0f0b2d5b317f532b6
jaasonw <jayywong92@gmail.com>
2022-05-26 17:52:53 -0700

    STRING

--- src/lib/blog/utils.test.ts
+++ src/lib/blog/utils.test.ts
@@ -3,11 +3,11 @@ import { readingTime } from './utils';
 import fs from 'fs';
 
 test('properly times plain text strings', () => {
-  const string = fs.readFileSync('src/lib/blog/bee-movie.txt').toString();
-  expect(readingTime(string)).toBe(41);
+  const sampleText = fs.readFileSync('src/lib/blog/bee-movie.txt').toString();
+  expect(readingTime(sampleText)).toBe(41);
 });
 
 test('properly times html strings', () => {
-  const string = fs.readFileSync('src/lib/blog/enchanted.html').toString();
-  expect(readingTime(string)).toBe(3);
+  const sampleText = fs.readFileSync('src/lib/blog/enchanted.html').toString();
+  expect(readingTime(sampleText)).toBe(3);
 });
