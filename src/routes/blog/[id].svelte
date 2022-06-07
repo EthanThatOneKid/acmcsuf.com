f9b65e892008b0334e197d78855c0ccb5d7ae123
jaasonw <jayywong92@gmail.com>
2022-06-06 19:33:13 -0700

    Remove commented code

--- src/routes/blog/[id].svelte
+++ src/routes/blog/[id].svelte
@@ -68,8 +68,6 @@
 </section>
 
 <style lang="scss">
-  // @import '$lib/blog/syntax-highlight.css';
-
   section {
     display: flex;
     flex-direction: column;
@@ -98,31 +96,6 @@
     filter: drop-shadow(0 8px 40px rgba(16, 19, 21, 0.1));
     -webkit-filter: drop-shadow(0 8px 40px rgba(16, 19, 21, 0.1));
     width: min(1000px, 70vw);
-
-    // .markdown-body {
-    //   text-align: left;
-
-    //   :global(p) {
-    //     margin-bottom: 16px;
-    //   }
-    //   :global(code),
-    //   :global(pre) {
-    //     font-family: monospace;
-    //     font-size: 1.5em;
-    //     background-color: var(--acm-canvas);
-    //     border-radius: 0.5em;
-    //     overflow-y: scroll;
-    //     margin: 0.5em 0;
-    //   }
-    //   :global(pre) {
-    //     padding: 16px;
-    //   }
-    //   :global(code) {
-    //     border-radius: 0.3em;
-    //     padding: 0.1em 0.5em;
-    //     margin: 0 0.3em;
-    //   }
-    // }
   }
   img {
     height: 100%;
