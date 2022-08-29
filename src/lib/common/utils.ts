4d40cb7e671a69de0a5811c1fc5df2512c4661d1
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-08-29 00:50:40 -0700

    This is true progress.

--- src/lib/common/utils.ts
+++ src/lib/common/utils.ts
@@ -1,12 +1,13 @@
-/** List of JavaScript expressions that evaluate to falsy. */
+/**
+ * Non-exhaustive list of JavaScript expressions that evaluate to falsy.
+ * @see https://developer.mozilla.org/en-US/docs/Glossary/Falsy/
+ */
 const FALSY_PRIMITIVES = ['0', '0.0', '0x0', 'null', 'undefined', 'false', 'NaN', "''", '""', '``'];
 
+/** RegExp pattern compiled from falsy primitives above. */
 const FALSY_PATTERN = new RegExp(`^(${FALSY_PRIMITIVES.join('|')})$`, 'i');
 
-/**
- * parseBool checks the boolean representation of a given string.
- * @see https://developer.mozilla.org/en-US/docs/Glossary/Falsy
- */
+/** parseBool checks the boolean representation of a given string. */
 export function parseBool(payload?: string | null): boolean {
   // It is easier to first check if the string is falsy, to determine the
   // boolean value of the string.
