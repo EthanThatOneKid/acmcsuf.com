a45ee873677358bd56db773aa5dafc46dd791d36
mle225 <mle225@csu.fullerton.edu>
2022-08-29 15:12:49 +0700

    Birth of `parseBool()` (#455)
    * added common util folder, implemented isStringTruthy, added isPinned to acmEvent
    
    * cast DEBUG to string to check
    
    * Update isStringTruthy function signature
    
    Co-authored-by: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
    
    * Update isStringTruthy truthy check
    
    Co-authored-by: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
    
    * Update src/lib/common/utils.ts
    
    Co-authored-by: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
    
    * modified isStringTruthy and added test cases
    
    * Refactored truthiness checker function
    
    - Renamed to `determineTruthyString`
    
    * Updated tests for `determineTruthyString`
    
    * Updated import for `determineTruthyString`
    
    * Attempt number 1 at _Recent Events_ UI
    
    * Added ICAL parser integration testing
    
    - Generated 10 events from the existing `events.ics` testdata file.
    - Created a test that verifies that the ICAL parser parses the code deterministically as expected.
    
    * Show sample events if in debug mode and no events currently exist
    
    * Progress
    
    * Updated sample event data
    
    * This commit should fail the tests, but it won't
    
    * Resolved <https://github.com/EthanThatOneKid/acmcsuf.com/pull/455#discussion_r876865340> and <https://github.com/EthanThatOneKid/acmcsuf.com/pull/455#discussion_r876865482>
    
    * Revert "Attempt number 1 at _Recent Events_ UI"
    
    This reverts commit af8c952a87cca83d84c19273a6349dd7490985de.
    
    * Renamed `determineTruthyString` to `parseBool`
    
    * Revert "This commit should fail the tests, but it won't"
    
    This reverts commit f6f85d18be593cd911067fc46eba4e3f45a40b5e.
    
    * Included a case for "any non-falsy string" since any string passed to parseBool that isn't falsy should return true.
    
    Resolves <https://github.com/EthanThatOneKid/acmcsuf.com/pull/455/files#r879979282>.
    
    * Refactored parseBool function
    
    Resolved <https://github.com/EthanThatOneKid/acmcsuf.com/pull/455/files#r878605080>.
    
    Resolved <https://github.com/EthanThatOneKid/acmcsuf.com/pull/455/files#r878605142>.
    
    * This is true progress.
    
    Co-authored-by: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>

---
+++ src/lib/common/utils.test.ts
@@ -0,0 +1,16 @@
+import { test, expect } from 'vitest';
+import * as utils from './utils';
+
+const TRUTHY_INPUT_DATA = ['tRuThY sTrInG', 'TRUE', String(true), String(1)];
+for (const input of TRUTHY_INPUT_DATA) {
+  test(`correctly determines input value '${input}' as truthy`, () => {
+    expect(utils.parseBool(input)).toBe(true);
+  });
+}
+
+const FALSY_INPUT_DATA = ['FALSE', '', null, undefined, String(false), String(NaN), String(0)];
+for (const input of FALSY_INPUT_DATA) {
+  test(`correctly determines input value '${input}' as falsy`, () => {
+    expect(utils.parseBool(input)).toBe(false);
+  });
+}
