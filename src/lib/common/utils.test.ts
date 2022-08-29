7b88b6be5fdff6bf021224e69fc0834b2adb490e
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-08-28 22:08:51 -0700

    Included a case for "any non-falsy string" since any string passed to parseBool that isn't falsy should return true.
    Resolves <https://github.com/EthanThatOneKid/acmcsuf.com/pull/455/files#r879979282>.

--- src/lib/common/utils.test.ts
+++ src/lib/common/utils.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from 'vitest';
 import * as utils from './utils';
 
-const TRUTHY_INPUT_DATA = ['TRUE', String(true), String(1)];
+const TRUTHY_INPUT_DATA = ['tRuThY sTrInG', 'TRUE', String(true), String(1)];
 for (const input of TRUTHY_INPUT_DATA) {
   test(`correctly determines input value '${input}' as truthy`, () => {
     expect(utils.parseBool(input)).toBe(true);
