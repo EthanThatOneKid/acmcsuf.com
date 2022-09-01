86484803214b9b3d1031b432565bf2dc3fa08bb1
jason wong <jayywong92@gmail.com>
2022-08-31 17:32:58 -0700

    Revert "Refactored caching logic for individual blog posts (#515)"
    This reverts commit 9df87d0c702d06497a09d05a1891f9034243adce.

--- src/routes/blog/[id].json.ts
+++ src/routes/blog/[id].json.ts
@@ -2,14 +2,18 @@ import type { RequestEvent, RequestHandlerOutput } from '@sveltejs/kit/types/int
 import type { Newsletter } from './_query';
 
 async function getCache(id: number, origin: string) {
-  const target = new URL('/blog.json', origin);
+  const target = origin + `/blog.json`;
 
-  const response = await fetch(target.toString());
-  const payload = await response.json();
-
-  if (payload.posts && payload.posts.length > 0) {
-    const posts = payload.posts as Newsletter[];
-    return posts.find((item) => item.id === id);
+  try {
+    const response = await fetch(target);
+    const data = await response.json();
+    const newsletter = (data as Newsletter[]).find((item) => {
+      return item.id === id;
+    });
+    return newsletter;
+  } catch (err) {
+    console.error(err);
+    return undefined;
   }
 }
 
