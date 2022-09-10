747c1de34feebb2d6ca81b5b71345318ccd2f986
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-10 12:04:11 -0700

    Stash everything from the project

--- .github/PULL_REQUEST_TEMPLATE/screenshots.md
+++
@@ -1,7 +0,0 @@
-## A picture tells a thousand words
-
-## Before this PR
-
-## After this PR
-
-<!-- source: https://github.com/stevemao/github-issue-templates/blob/master/screenshots/PULL_REQUEST_TEMPLATE.md >
--- src/lib/qr/utils.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from 'vitest';
-import { normalizeLinkName } from '$lib/qr/utils';
-
-test('cleans shortlinks', () => {
-  const actual = normalizeLinkName('My-Example');
-  expect(actual).toBe('my-example');
-});
