a03b6bfce1fff47c361df1c298333dad2f4d64a8
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-10 15:01:55 -0700

    Added test for index page

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
-  expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+  expect(await page.textContent('h1')).toBe(
+    'We are the largest computer science community at CSUF'
+  );
 });
