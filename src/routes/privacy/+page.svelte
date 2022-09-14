28e2fe96e2a49d2c5a2c91861f47debfd766862b
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-13 16:22:27 -0800

    SvelteKit release candidate migration: privacy page
    - Moved [`src/lib/components/privacy/privacy-policy.svelte`](https://github.com/EthanThatOneKid/acmcsuf.com/blob/36d15b5cdf373b3c594c773ebf3b3942e54677c6/src/lib/components/privacy/privacy-policy.svelte) to `src/routes/privacy/policy.svelte`.

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  // TODO: Replace Spacing [and CommonHero] with Block
+  import Block from '$lib/components/block/block.svelte';
+  import Spacing from '$lib/legacy/spacing.svelte';
+  import PrivacyPolicy from './policy.svelte';
+</script>
+
+<Spacing --min="175px" --med="200px" --max="200px" />
+
+<Block>
+  <h2 slot="headline" class="size-lg">Privacy Policy</h2>
+  <p slot="text" class="size-sm">
+    Our privacy policy describes how our Discord bot collects, uses, and shares your personal data.
+    <br />
+    <br />
+    Last updated January 16th, 2022
+  </p>
+</Block>
+
+<Spacing --med="64px" />
+
+<PrivacyPolicy />
+
+<Spacing --min="40px" --med="95px" --max="120px" />
