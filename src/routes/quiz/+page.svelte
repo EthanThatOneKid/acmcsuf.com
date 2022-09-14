76feb20629497a89275ccfed4fc5beac2a429688
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-13 17:22:25 -0800

    SvelteKit release candidate migration: quiz page
    - Resolved errors in `quiz.svelte` refactor

---
+++ src/routes/quiz/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Spacing from '$lib/legacy/spacing.svelte';
+
+  import { QUIZ_DATA } from './data';
+  import Quiz from './quiz.svelte';
+</script>
+
+<Spacing --min="175px" --med="200px" --max="200px" />
+
+<Quiz data={QUIZ_DATA} />
+
+<Spacing --min="175px" --med="200px" --max="200px" />
