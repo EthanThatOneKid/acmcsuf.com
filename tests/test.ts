df8af18291ef32534590dd22f05ef8ae5394ed64
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-14 19:40:16 -0800

    Progress
    TODO: figure out how to make Playwright ONLY test in `/tests/`.

--- tests/test.ts
+++ tests/test.ts
@@ -6,3 +6,8 @@ test('index page has expected h1', async ({ page }) => {
     'We are the largest computer science community at CSUF'
   );
 });
+
+test('about page has expected h1', async ({ page }) => {
+  await page.goto('/about');
+  expect(await page.textContent('h1')).toBe('About us');
+});
