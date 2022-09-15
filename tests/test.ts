5e33be247794cdf44f533a9289dcc68dbe2574e1
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-14 19:58:30 -0800

    Specified `/tests` for Playwright testing

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
-  expect(await page.textContent('h1')).toBe(
-    'We are the largest computer science community at CSUF'
-  );
+  expect(await page.textContent('h1')).toContain('We are the largest computer science community');
 });
