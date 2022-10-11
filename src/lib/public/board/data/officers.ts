177c8fd3de36024907be8cde2f885d934c24dbd3
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-10-11 00:42:13 -0700

    Create officers.ts

---
+++ src/lib/public/board/data/officers.ts
@@ -0,0 +1,707 @@
+import type { Officer } from '$lib/public/board/types';
+import { Tier } from '$lib/public/board/types';
+
+export const OFFICERS: Officer[] = [
+  {
+    fullName: 'Armanul Ambia',
+    picture: 'armanul-ambia.webp',
+    positions: {
+      S21: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+      F21: {
+        title: 'Algo Director',
+        tier: Tier.ALGO_LEADER,
+      },
+      S22: {
+        title: 'Algo President',
+        tier: Tier.ALGO_LEADER,
+      },
+      F22: {
+        title: 'Algo Team Lead',
+        tier: Tier.ALGO_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Jason Anthony',
+    picture: 'jason-anthony.webp',
+    positions: {
+      S21: {
+        title: 'Secretary',
+        tier: Tier.SECRETARY,
+      },
+    },
+  },
+  {
+    fullName: 'Angel Armendariz',
+    picture: 'angel-armendariz.webp',
+    positions: {
+      S22: {
+        title: 'Dev Project Manager',
+        tier: Tier.DEV_PROJECT_MANAGER,
+      },
+    },
+  },
+  {
+    fullName: 'Sami Bajwa',
+    picture: 'sami-bajwa.webp',
+    positions: {
+      F21: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+      S22: {
+        title: 'Algo Officer, nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+      F22: {
+        title: 'Algo Officer',
+        tier: Tier.ALGO_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Johnathan Carranza',
+    picture: 'johnathan-carranza.webp',
+    positions: {
+      F21: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Alan Cortez',
+    picture: 'alan-cortez.webp',
+    positions: {
+      S22: {
+        title: 'Create Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+      F22: {
+        title: 'Design Team Lead',
+        tier: Tier.DESIGN_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Wesley Chou',
+    picture: 'wesley-chou.webp',
+    positions: {
+      S21: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+      F21: {
+        title: 'Dev Director',
+        tier: Tier.DESIGN_LEADER,
+      },
+      S22: {
+        title: 'Dev President',
+        tier: Tier.DEV_LEADER,
+      },
+      F22: {
+        title: 'Marketing Co-Lead',
+        tier: Tier.MARKETING_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Ethan Davidson',
+    picture: 'ethan-davidson.webp',
+    positions: {
+      S21: {
+        title: 'Webmaster',
+        tier: Tier.WEBMASTER,
+      },
+      F21: {
+        title: 'Webmaster',
+        tier: Tier.WEBMASTER,
+      },
+      S22: {
+        title: 'Webmaster',
+        tier: Tier.WEBMASTER,
+      },
+      F22: {
+        title: 'Vice President/Webmaster',
+        tier: Tier.VICE_PRESIDENT,
+      },
+    },
+  },
+  {
+    fullName: 'Kevin Dillon',
+    picture: 'kevin-dillon.webp',
+    positions: {
+      S21: {
+        title: 'Algo Officer',
+        tier: Tier.ALGO_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Diamond Dinh',
+    picture: 'diamond-dinh.webp',
+    positions: {
+      S22: {
+        title: 'Dev Project Manager',
+        tier: Tier.DEV_PROJECT_MANAGER,
+      },
+      F22: {
+        title: 'Special Events Officer',
+        tier: Tier.SPECIAL_EVENTS_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Eduardo Gomez',
+    picture: 'eduardo-gomez.webp',
+    positions: {
+      S21: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Nurhaliza Hassan',
+    picture: 'nurhaliza-hassan.webp',
+    positions: {
+      S22: {
+        title: 'Marketing Director',
+        tier: Tier.MARKETING_LEADER,
+      },
+      F22: {
+        title: 'Marketing Co-Lead',
+        tier: Tier.MARKETING_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Lisa Hong',
+    picture: 'lisa-hong.webp',
+    positions: {
+      S21: {
+        title: 'Create Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Joshua Hughes',
+    picture: 'joshua-hughes.webp',
+    positions: {
+      S21: {
+        title: 'ICC Representative',
+        tier: Tier.SECRETARY,
+      },
+    },
+  },
+  {
+    fullName: 'Iftekharul Islam',
+    picture: 'iftekharul-islam.webp',
+    positions: {
+      S22: {
+        title: 'Algo Officer',
+        tier: Tier.ALGO_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Ibrahim Israr',
+    picture: 'ibrahim-israr.webp',
+    positions: {
+      S22: {
+        title: 'Secretary',
+        tier: Tier.SECRETARY,
+      },
+    },
+  },
+  {
+    fullName: 'Joel Anil John',
+    picture: 'joel-anil-john.webp',
+    positions: {
+      S22: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+      F22: {
+        title: 'Algo Officer',
+        tier: Tier.ALGO_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Andy Lasso',
+    picture: 'andy-lasso.webp',
+    positions: {
+      F21: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Andrew Lau',
+    picture: 'andrew-lau.webp',
+    positions: {
+      S21: {
+        title: 'Treasurer',
+        tier: Tier.TREASURER,
+      },
+    },
+  },
+  {
+    fullName: 'Minh Le',
+    picture: 'minh-le.webp',
+    positions: {
+      S22: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Nguyen Le',
+    picture: 'nguyen-le.webp',
+    positions: {
+      S22: {
+        title: 'Data Analyst',
+        tier: Tier.DATA_ANALYST,
+      },
+    },
+  },
+  {
+    fullName: 'Tommy Le',
+    picture: 'tommy-le.webp',
+    positions: {
+      F21: {
+        title: 'Treasurer',
+        tier: Tier.TREASURER,
+      },
+      S22: {
+        title: 'Treasurer',
+        tier: Tier.TREASURER,
+      },
+    },
+  },
+  {
+    fullName: 'Eugene Lee',
+    picture: 'eugene-lee.webp',
+    positions: {
+      S21: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+      F21: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+      S22: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Nolan Lee',
+    picture: 'nolan-lee.webp',
+    positions: {
+      S22: {
+        title: 'Historian',
+        tier: Tier.HISTORIAN,
+      },
+    },
+  },
+  {
+    fullName: 'Aaron Lieberman',
+    picture: 'aaron-lieberman.webp',
+    positions: {
+      S21: {
+        title: 'Internal Vice President, nodebuds Officer',
+        tier: Tier.VICE_PRESIDENT,
+      },
+      F21: {
+        title: 'President, nodebuds Officer',
+        tier: Tier.PRESIDENT,
+      },
+      S22: {
+        title: 'President, nodebuds Officer',
+        tier: Tier.PRESIDENT,
+      },
+      F22: {
+        title: 'Special Events Team Lead',
+        tier: Tier.SPECIAL_EVENTS_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Shaleen Mathur',
+    picture: 'shaleen-mathur.webp',
+    positions: {
+      S21: {
+        title: 'Workshop Manager',
+        tier: Tier.DEV_PROJECT_MANAGER,
+      },
+    },
+  },
+  {
+    fullName: 'Ean McGilvery',
+    picture: 'ean-mcgilvery.webp',
+    positions: {
+      S21: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Jorge Mejia',
+    picture: 'jorge-mejia.webp',
+    positions: {
+      F21: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+      S22: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+      F22: {
+        title: 'Design Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Serena Naranjo',
+    picture: 'serena-naranjo.webp',
+    positions: {
+      F21: {
+        title: 'Create Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+      S22: {
+        title: 'Create Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Dalisa Nguyen',
+    picture: 'dalisa-nguyen.webp',
+    positions: {
+      S21: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Kayla Nguyen',
+    picture: 'kayla-nguyen.webp',
+    positions: {
+      F21: {
+        title: 'Create Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Jacob Nguyen',
+    picture: 'jacob-nguyen.webp',
+    positions: {
+      S21: {
+        title: 'President, Create Director, nodebuds Officer',
+        tier: Tier.PRESIDENT,
+      },
+      F22: {
+        title: 'Treasurer',
+        tier: Tier.TREASURER,
+      },
+    },
+  },
+  {
+    fullName: 'Taylor Noh',
+    picture: 'taylor-noh.webp',
+    positions: {
+      S21: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+      F21: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+      S22: {
+        title: 'nodebuds Officer',
+        tier: Tier.NODEBUDS_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Kirsten Ochoa',
+    picture: 'kirsten-ochoa.webp',
+    positions: {
+      S22: {
+        title: 'Create Project Developer',
+        tier: Tier.DESIGN_PROJECT_MANAGER,
+      },
+      F22: {
+        title: 'Design Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Mike Ploythai',
+    picture: 'mike-ploythai.webp',
+    positions: {
+      S21: {
+        title: 'Create Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+      F21: {
+        title: 'Create Director, Marketing Chair',
+        tier: Tier.DESIGN_LEADER,
+      },
+      S22: {
+        title: 'Create President',
+        tier: Tier.DESIGN_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Stephanie Pocci',
+    picture: 'stephanie-pocci.webp',
+    positions: {
+      S22: {
+        title: 'Event Coordinator',
+        tier: Tier.EVENT_COORDINATOR,
+      },
+      F22: {
+        title: 'Event Coordinator',
+        tier: Tier.EVENT_COORDINATOR,
+      },
+    },
+  },
+  {
+    fullName: 'Nicolas Renteria',
+    picture: 'nicolas-renteria.webp',
+    positions: {
+      S21: {
+        title: 'Marketing Chair',
+        tier: Tier.MARKETING_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Wilbert Rodriguez',
+    picture: 'wilbert-rodriguez.webp',
+    positions: {
+      S21: {
+        title: 'Intern Program Manager',
+        tier: Tier.DEV_PROJECT_MANAGER,
+      },
+    },
+  },
+  {
+    fullName: 'Samuel Sandoval',
+    picture: 'samuel-sandoval.webp',
+    positions: {
+      S21: {
+        title: 'Vice President, Dev Director',
+        tier: Tier.VICE_PRESIDENT,
+      },
+    },
+  },
+  {
+    fullName: 'Kavit Sanghavi',
+    picture: 'kavit-sanghavi.webp',
+    positions: {
+      S21: {
+        title: 'Algo Director',
+        tier: Tier.ALGO_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Parth Sharma',
+    picture: 'parth-sharma.webp',
+    positions: {
+      S21: {
+        title: 'Algo Officer',
+        tier: Tier.ALGO_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Justin Stitt',
+    picture: 'justin-stitt.webp',
+    positions: {
+      S22: {
+        title: 'Algo Officer',
+        tier: Tier.ALGO_OFFICER,
+      },
+      F22: {
+        title: 'AI Team Lead',
+        tier: Tier.AI_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Johnson Tong',
+    picture: 'johnson-tong.webp',
+    positions: {
+      S21: {
+        title: 'Workshop Manager',
+        tier: Tier.DEV_PROJECT_MANAGER,
+      },
+    },
+  },
+  {
+    fullName: 'Alex Truong',
+    positions: {
+      F21: {
+        title: 'Algo Officer',
+        tier: Tier.ALGO_OFFICER,
+      },
+    },
+    picture: 'alex-truong.webp',
+  },
+  {
+    fullName: 'Daniel Truong',
+    picture: 'daniel-truong.webp',
+    positions: {
+      S22: {
+        title: 'Dev Project Manager',
+        tier: Tier.DEV_PROJECT_MANAGER,
+      },
+      F22: {
+        title: 'Dev Project Manager',
+        tier: Tier.DEV_PROJECT_MANAGER,
+      },
+    },
+  },
+  {
+    fullName: 'Samuel Valls',
+    picture: 'samuel-valls.webp',
+    positions: {
+      S21: {
+        title: 'Community Manager',
+        tier: Tier.MARKETING_LEADER,
+      },
+      F21: {
+        title: 'Create Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+      S22: {
+        title: 'Create Operations Manager',
+        tier: Tier.DESIGN_OPERATIONS_MANAGER,
+      },
+    },
+  },
+  {
+    fullName: 'Karnikaa Velumani',
+    picture: 'karnikaa-velumani.webp',
+    positions: {
+      F21: {
+        title: 'Vice President',
+        tier: Tier.VICE_PRESIDENT,
+      },
+      S22: {
+        title: 'Vice President',
+        tier: Tier.VICE_PRESIDENT,
+      },
+      F22: {
+        title: 'President',
+        tier: Tier.PRESIDENT,
+      },
+    },
+  },
+  {
+    fullName: 'Rina Watanabe',
+    picture: 'rina-watanabe.webp',
+    positions: {
+      F21: {
+        title: 'Dev Project Manager',
+        tier: Tier.DEV_PROJECT_MANAGER,
+      },
+    },
+  },
+  {
+    fullName: 'Jason Wong',
+    picture: 'jason-wong.webp',
+    positions: {
+      S22: {
+        title: 'Historian',
+        tier: Tier.HISTORIAN,
+      },
+      F22: {
+        title: 'Historian',
+        tier: Tier.HISTORIAN,
+      },
+    },
+  },
+  {
+    fullName: 'Alejandro Ramos',
+    picture: 'alejandro-ramos.webp',
+    positions: {
+      F22: {
+        title: 'Design Officer',
+        tier: Tier.DESIGN_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Angel Santoyo',
+    picture: 'angel-santoyo.webp',
+    positions: {
+      F22: {
+        title: 'Dev Officer',
+        tier: Tier.DEV_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Charlie Taylor',
+    picture: 'charlie-taylor.webp',
+    positions: {
+      F22: {
+        title: 'Dev Team Lead',
+        tier: Tier.DEV_LEADER,
+      },
+    },
+  },
+  {
+    fullName: 'Yao Lin',
+    picture: 'yao-lin.webp',
+    positions: {
+      F22: {
+        title: 'AI Officer',
+        tier: Tier.AI_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Sara Sadek',
+    picture: 'placeholder.webp',
+    positions: {
+      F22: {
+        title: 'AI Officer',
+        tier: Tier.AI_OFFICER,
+      },
+    },
+  },
+  {
+    fullName: 'Arish Imam',
+    picture: 'placeholder.webp',
+    positions: {
+      F22: {
+        title: 'AI Officer',
+        tier: Tier.AI_OFFICER,
+      },
+    },
+  },
+];
