5ea8a2461f02b01cd307c8277290cf4cb59a3e31
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-10-19 15:06:12 -0700

    Removed screenshot tests for `/event` page
    Removed screenshot tests for `/event` page
    
    TODO: Run Playwright tests on another computer to make sure the tests are behaving as expected.

--- src/routes/(site)/events/page.test.ts
+++ src/routes/(site)/events/page.test.ts
@@ -1,58 +1,6 @@
-import type { TestInfo } from '@playwright/test';
 import { expect, test } from '@playwright/test';
-import path from 'node:path';
-
-test.describe.configure({ mode: 'parallel' });
-
-// setupSnapshot is a helper that creates handles the necessary boilerplate
-// for snapshot testing. It returns a function that can be called after the
-// snapshot test has executed.
-//
-// See:
-// https://github.com/microsoft/playwright/issues/14527#issuecomment-1143352769
-export function setupSnapshot(testInfo: TestInfo) {
-  const snapshotDir = testInfo.snapshotDir;
-  const snapshotSuffix = testInfo.snapshotSuffix;
-  const projectName = testInfo.project.name;
-
-  testInfo.snapshotDir = path.resolve(snapshotDir, '..', 'screenshots');
-  testInfo.snapshotSuffix = '';
-  testInfo.project.name = '';
-
-  return {
-    data: { projectName },
-    cleanupSnapshot() {
-      testInfo.snapshotDir = snapshotDir;
-      testInfo.snapshotSuffix = snapshotSuffix;
-      testInfo.project.name = projectName;
-    },
-  };
-}
 
 test('events page has expected h2', async ({ page }) => {
   await page.goto('/events');
   expect(await page.textContent('h2')).toBe('Curated events for growth and success');
-});
-
-test('events page matches screenshot', async ({ page }, testInfo) => {
-  const { cleanupSnapshot, data } = setupSnapshot(testInfo);
-
-  if (testInfo.project.use.viewport) {
-    await page.setViewportSize(testInfo.project.use.viewport);
-  }
-
-  if (testInfo.project.use.colorScheme) {
-    await page.emulateMedia({
-      colorScheme: testInfo.project.use.colorScheme,
-    });
-  }
-
-  await page.goto('/events', { waitUntil: 'networkidle' });
-  expect(await page.screenshot({ fullPage: true })).toMatchSnapshot({
-    name: `page-${data.projectName}.png`,
-    maxDiffPixelRatio: 0.1,
-    threshold: 1,
-  });
-
-  cleanupSnapshot();
 });
