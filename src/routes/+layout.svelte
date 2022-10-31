d461e7ec94fc186c722923ae19a40e86da6b1546
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-10-30 21:34:35 -0800

    Revert "Analytics: Add Vercel Audiences (#649)"
    This reverts commit 6fd01c74685f3f23a621c3a0ddb6eb0efe5546ac.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,24 +5,17 @@
   //
   // https://kit.svelte.dev/docs/advanced-routing#advanced-layouts
 
-  import { inject } from '@vercel/analytics';
   import { page } from '$app/stores';
   import { browser, dev } from '$app/environment';
   import { VERCEL_ANALYTICS_ID } from '$env/static/public';
   import { send } from '$lib/public/analytics/vitals';
 
   $: if (browser && !dev && VERCEL_ANALYTICS_ID) {
-    inject({
-      beforeSend() {
-        send({
-          id: VERCEL_ANALYTICS_ID,
-          path: $page.url.pathname,
-          params: $page.params,
-          navigator,
-        });
-
-        return null;
-      },
+    send({
+      id: VERCEL_ANALYTICS_ID,
+      path: $page.url.pathname,
+      params: $page.params,
+      navigator,
     });
   }
 </script>
