09ae75b96d7303e9507104b72233c35ccdc85504
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-11-08 10:42:04 -0800

    Update data.ts

--- src/lib/server/links/data.ts
+++ src/lib/server/links/data.ts
@@ -8,10 +8,15 @@ export const LINKS = {
   'acm-w': 'https://sites.google.com/view/acm-w',
   'covid-19':
     'https://docs.google.com/document/d/1aEfH50N0fmeK3JiID968DoG41jmdk3nelDipQpF6yio/edit?usp=sharing',
-  bug: 'https://github.com/EthanThatOneKid/acmcsuf.com/issues/new?assignees=&labels=bug&template=bug_report.yaml',
-  enhance:
-    'https://github.com/EthanThatOneKid/acmcsuf.com/issues/new?assignees=&labels=enhancement&template=feature_request.yaml',
-  gfi: 'https://github.com/EthanThatOneKid/acmcsuf.com/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22',
+  issues: '/github/issues',
+  bug: '/issues/new?assignees=&labels=bug&template=bug_report.yaml',
+  enhance: '/issues/new?assignees=&labels=enhancement&template=feature_request.yaml',
+  gfi: '/issues?q=is%3Aopen+is%3Aissue+label%3Abulbasaur%2Csquirtle%2Ccharmander',
+  'gfi/front': '/issues?q=is%3Aopen+is%3Aissue+label%3Asquirtle',
+  'gfi/back': '/issues?q=is%3Aopen+is%3Aissue+label%3Abulbasaur',
+  'gfi/misc': '/issues?q=is%3Aopen+is%3Aissue+label%3Acharmander',
+  pull: '/github/pull',
+  'blog/new': '/github/discussions/new?category=Newsletter',
   devform: 'https://forms.gle/Uu8bbHkuVCRspxZK8',
   devdeck:
     'https://docs.google.com/presentation/d/1I2iuN2h1awuqtd2osNo3kYuxdsYGOEwGYC1zn9zHtvs/edit?usp=sharing',
@@ -30,4 +35,9 @@ export const LINKS = {
   botform:
     'https://docs.google.com/forms/d/e/1FAIpQLSe07zvJR8kfjWXWIDilIMdTzjenCeiFdAGTjX81dcI6XotE5Q/viewform',
   'intro-to-python': 'https://colab.research.google.com/drive/1oXXibZCxEnFnyCfEnxk9x-Qcgp1n4zJa',
+  'hash-table-visu': 'https://hash-table-visu.vercel.app',
+  'ai/projects/1':
+    'https://docs.google.com/presentation/d/1BCYU3cTfNTffdUmiMHz8c2-A5qwna8yVPt7Fx3nzqGQ/edit?usp=sharing',
+  'ai/projects/2':
+    'https://colab.research.google.com/drive/1KaLN47LSfK4rAOzEOQleCX-Dy-qACZYC?usp=sharing',
 } as const;
