018a84139ece51c007a5dd8e82e820806c5a763c
Joe Lee <lefodev@gmail.com>
2022-11-11 16:53:25 -0800

    Added redirect button to 404 page and fixed flex gap

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,13 +9,31 @@
 <section title={$page.error?.message}>
   <em>404</em>
   <h1>Frank can't find where you're going!</h1>
+  <a href="/">Return to Home</a>
   <img src="/assets/png/lost-frank.png" alt="404 - Page Not Found" />
 </section>
 
 <style>
+  a {
+    display: block;
+    text-align: center;
+    padding: 12px 24px;
+    background-color: var(--button-bg);
+    color: var(--perma-light);
+    border-radius: 12px;
+    text-decoration: none;
+    user-select: none;
+    transition: 0.25s ease-in-out;
+  }
+
+  a:hover {
+    background-color: var(--button-hover);
+  }
+
   section {
     min-height: 100vh;
     display: flex;
+    gap: 2rem;
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -24,7 +42,6 @@
   }
 
   section h1 {
-    margin-bottom: 2em;
     align-items: center;
     text-align: center;
   }
@@ -33,8 +50,6 @@
     align-items: center;
     text-align: center;
     font-size: 50px;
-    margin-bottom: 0.5em;
-    margin-top: 2em;
     text-shadow: 2px 2px #92c4df;
     font-weight: 600;
   }
@@ -43,6 +58,5 @@
     width: 100%;
     max-width: 1000px;
     height: auto;
-    margin-bottom: 3em;
   }
 </style>
