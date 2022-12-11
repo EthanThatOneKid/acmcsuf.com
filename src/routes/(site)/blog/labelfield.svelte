ecdd7aa09cbf709b1cde965bea908a60bbeac93b
davids700 <dsolano7@csu.fullerton.edu>
2022-12-10 19:48:06 -0800

    Duplicated Changes
    Changed the color of the labels in labelfield.svelte to match acm colors.

--- src/routes/(site)/blog/labelfield.svelte
+++ src/routes/(site)/blog/labelfield.svelte
@@ -91,8 +91,8 @@
         margin-bottom: 0.2em;
         margin-right: var(--size-sm);
         padding: 0.25em 0.8em 0.25em 0.8em;
-        background-color: #eeeeee;
-        color: #212121;
+        background-color: var(--acm-light);
+        color: var(--acm-dark);
         border-radius: var(--size-sm);
         border: 2px solid #e0e0e0;
         cursor: pointer;
