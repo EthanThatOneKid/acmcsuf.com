63aca6305275dc243181ea35424200d5c91b70fc
David Solano <dsolano7@csu.fullerton.edu>
2023-01-17 00:17:35 -0800

    Create Shortlinks page
    New minimalist page including all the acmcsuf.org short links. This PR addresses enhancement #711.

---
+++ src/routes/(site)/shorter/+page.svelte
@@ -0,0 +1,253 @@
+<script context="module" lang="ts">
+</script>
+
+<script lang="ts">
+  import Spacing from '$lib/public/legacy/spacing.svelte';
+</script>
+
+<Spacing --min="175px" --med="200px" --max="200px" />
+
+<svelte:head>
+  <title>Shortlinks | ACM at CSUF</title>
+</svelte:head>
+
+<main class="responsive-width">
+  <section id="top">
+    <div class="header">
+      <h1 class="size-lg">Shortlinks</h1>
+    </div>
+    <p class="brand-em">
+      github:
+      <a href="https://github.com/EthanThatOneKid/acmcsuf.com"
+        >https://github.com/EthanThatOneKid/acmcsuf.com</a
+      >
+      <br />
+      <br />
+      youtube:
+      <a href="https://www.youtube.com/channel/UCDMBj0o4V8Cpt0jgsZLNVVg"
+        >https://www.youtube.com/channel/UCDMBj0o4V8Cpt0jgsZLNVVg</a
+      >
+      <br />
+      <br />
+      instagram:
+      <a href="https://instagram.com/acmcsuf">https://instagram.com/acmcsuf</a>
+      <br />
+      <br />
+      discord:
+      <a href="https://discord.gg/eKRApG627b">https://discord.gg/eKRApG627b</a>
+      <br />
+      <br />
+      linkedin:
+      <a href="https://linkedin.com/company/acm-at-csuf/"
+        >https://linkedin.com/company/acm-at-csuf/</a
+      >
+      <br />
+      <br />
+      tuffyhacks:
+      <a href="https://tuffyhacks.com/">https://tuffyhacks.com/</a>
+      <br />
+      <br />
+      'acm-w':
+      <a href="https://sites.google.com/view/acm-w">https://sites.google.com/view/acm-w</a>
+      <br />
+      <br />
+      'covid-19':
+      <a
+        href="https://docs.google.com/document/d/1aEfH50N0fmeK3JiID968DoG41jmdk3nelDipQpF6yio/edit?usp=sharing"
+        >https://docs.google.com/document/d/1aEfH50N0fmeK3JiID968DoG41jmdk3nelDipQpF6yio/edit?usp=sharing</a
+      >
+      <br />
+      <br />
+      issues:
+      <a href="/github/issues">/github/issues</a>
+      <br />
+      <br />
+      bug:
+      <a href="/issues/new?assignees=&labels=bug&template=bug_report.yaml"
+        >/issues/new?assignees=&labels=bug&template=bug_report.yaml</a
+      >
+      <br />
+      <br />
+      enhance:
+      <a href="/issues/new?assignees=&labels=enhancement&template=feature_request.yaml"
+        >/issues/new?assignees=&labels=enhancement&template=feature_request.yaml</a
+      >
+      <br />
+      <br />
+      gfi:
+      <a href="/issues?q=is%3Aopen+is%3Aissue+label%3Abulbasaur%2Csquirtle%2Ccharmander"
+        >/issues?q=is%3Aopen+is%3Aissue+label%3Abulbasaur%2Csquirtle%2Ccharmander</a
+      >
+      <br />
+      <br />
+      'gfi/front':
+      <a href="/issues?q=is%3Aopen+is%3Aissue+label%3Asquirtle"
+        >/issues?q=is%3Aopen+is%3Aissue+label%3Asquirtle</a
+      >
+      <br />
+      <br />
+      'gfi/back':
+      <a href="/issues?q=is%3Aopen+is%3Aissue+label%3Abulbasaur"
+        >/issues?q=is%3Aopen+is%3Aissue+label%3Abulbasaur</a
+      >
+      <br />
+      <br />
+      'gfi/misc':
+      <a href="/issues?q=is%3Aopen+is%3Aissue+label%3Acharmander"
+        >/issues?q=is%3Aopen+is%3Aissue+label%3Acharmander</a
+      >
+      <br />
+      <br />
+      pull:
+      <a href="/github/pull">/github/pull</a>
+      <br />
+      <br />
+      'blog/new':
+      <a href="/github/discussions/new?category=Newsletter"
+        >/github/discussions/new?category=Newsletter</a
+      >
+      <br />
+      <br />
+      devform:
+      <a href="https://forms.gle/Uu8bbHkuVCRspxZK8">https://forms.gle/Uu8bbHkuVCRspxZK8</a>
+      <br />
+      <br />
+      devdeck:
+      <a
+        href="https://docs.google.com/presentation/d/1I2iuN2h1awuqtd2osNo3kYuxdsYGOEwGYC1zn9zHtvs/edit?usp=sharing"
+        >https://docs.google.com/presentation/d/1I2iuN2h1awuqtd2osNo3kYuxdsYGOEwGYC1zn9zHtvs/edit?usp=sharing</a
+      >
+      <br />
+      <br />
+      'design-template':
+      <a
+        href="https://www.figma.com/file/MED3kRKmIxcoKblNTveEZZ/acmDesign-Challenge-Sheet?node-id=0%3A1"
+        >https://www.figma.com/file/MED3kRKmIxcoKblNTveEZZ/acmDesign-Challenge-Sheet?node-id=0%3A1</a
+      >
+      <br />
+      <br />
+      'live-walk':
+      <a href="https://www.figma.com/file/PtYSxzB8BbKh3L46dQSjf1/Live-Interaction?node-id=0%3A1"
+        >https://www.figma.com/file/PtYSxzB8BbKh3L46dQSjf1/Live-Interaction?node-id=0%3A1</a
+      >
+      <br />
+      <br />
+      'css-basics':
+      <a href="https://codepen.io/alancortez1337/pen/BaxmJWL"
+        >https://codepen.io/alancortez1337/pen/BaxmJWL</a
+      >
+      <br />
+      <br />
+      'grid-play':
+      <a href="https://codepen.io/jorgeemejia/pen/MWGBdQj"
+        >https://codepen.io/jorgeemejia/pen/MWGBdQj</a
+      >
+      <br />
+      <br />
+      'frank-grid':
+      <a href="https://codepen.io/alancortez1337/pen/xxjPVbG"
+        >https://codepen.io/alancortez1337/pen/xxjPVbG</a
+      >
+      <br />
+      <br />
+      arch:
+      <a href="/github/blob/main/ARCHITECTURE.md">/github/blob/main/ARCHITECTURE.md</a>
+      <br />
+      <br />
+      'game-night':
+      <a href="https://discord.com/events/710225099923521558/1031436363259723817"
+        >https://discord.com/events/710225099923521558/1031436363259723817s</a
+      >
+      <br />
+      <br />
+      webdevform:
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSflnzKjgYKmNOkwIYRVveGVGuzso4Fm9Ph0gxAULgt6w5Xx9A/viewform"
+        >https://docs.google.com/forms/d/e/1FAIpQLSflnzKjgYKmNOkwIYRVveGVGuzso4Fm9Ph0gxAULgt6w5Xx9A/viewform</a
+      >
+      <br />
+      <br />
+      icpcform:
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSf_z4XjkpKQMNBGWArGZswmTmn38teFjsx-MjtT8xV-cVW-sQ/viewform"
+        >https://docs.google.com/forms/d/e/1FAIpQLSf_z4XjkpKQMNBGWArGZswmTmn38teFjsx-MjtT8xV-cVW-sQ/viewform</a
+      >
+      <br />
+      <br />
+      botform:
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSe07zvJR8kfjWXWIDilIMdTzjenCeiFdAGTjX81dcI6XotE5Q/viewform"
+        >https://docs.google.com/forms/d/e/1FAIpQLSe07zvJR8kfjWXWIDilIMdTzjenCeiFdAGTjX81dcI6XotE5Q/viewform</a
+      >
+      <br />
+      <br />
+      'intro-to-python':
+      <a href="https://colab.research.google.com/drive/1oXXibZCxEnFnyCfEnxk9x-Qcgp1n4zJa"
+        >https://colab.research.google.com/drive/1oXXibZCxEnFnyCfEnxk9x-Qcgp1n4zJa</a
+      >
+      <br />
+      <br />
+      'hash-table-visu':
+      <a href="https://hash-table-visu.vercel.app">https://hash-table-visu.vercel.app</a>
+      <br />
+      <br />
+      ai/projects/1:
+      <a
+        href="https://docs.google.com/presentation/d/1BCYU3cTfNTffdUmiMHz8c2-A5qwna8yVPt7Fx3nzqGQ/edit?usp=sharing"
+        >https://docs.google.com/presentation/d/1BCYU3cTfNTffdUmiMHz8c2-A5qwna8yVPt7Fx3nzqGQ/edit?usp=sharing</a
+      >
+      <br />
+      <br />
+      'ai/projects/2':
+      <a
+        href="https://colab.research.google.com/drive/1KaLN47LSfK4rAOzEOQleCX-Dy-qACZYC?usp=sharing"
+        >https://colab.research.google.com/drive/1KaLN47LSfK4rAOzEOQleCX-Dy-qACZYC?usp=sharing</a
+      >
+    </p>
+  </section>
+</main>
+
+<Spacing --min="40px" --med="95px" --max="120px" />
+
+<style lang="scss">
+  .responsive-width {
+    width: min(800px, 80vw);
+    max-width: 540px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+  }
+
+  main {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    section {
+      width: min(800px, 80vw);
+      max-width: 540px;
+      margin: 0 auto;
+      // padding: 0 1rem 6rem;
+      margin-bottom: 50px;
+
+      p {
+        margin: 1em 0;
+      }
+
+      a {
+        transition: 0.25s ease-in-out;
+
+        &:hover {
+          color: var(--acm-blue);
+        }
+      }
+    }
+  }
+</style>
