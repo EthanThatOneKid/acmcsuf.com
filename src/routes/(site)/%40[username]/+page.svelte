d556bcfa7edee60c3a1ea6066ec89ee6c6230a29
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-01-22 15:30:47 -0800

    Add OpenGraph page description

--- src/routes/(site)/%40[username]/+page.svelte
+++ src/routes/(site)/%40[username]/+page.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
   import type { PageData } from './$types';
   import { MetaTags } from 'svelte-meta-tags';
+  import { page } from '$app/stores';
+
   import Spacing from '$lib/public/legacy/spacing.svelte';
   import { makeCertificatePageUrl } from '$lib/public/certificates';
 
   export let data: PageData;
+
+  const pageTitle = `${data.certificate.to.tagName} acmcsuf.com contributions | ${data.certificate.user.name}`;
+
+  function makeDescription(cert: PageData['certificate']): string {
+    const { user, to, merged } = cert;
+    const { name, login } = user;
+    const { tagName } = to;
+
+    const contributions =
+      merged.length > 0
+        ? `made ${merged.length} contribution${merged.length > 1 ? 's' : ''}`
+        : 'did not make a contribution';
+
+    return `${name} (${login}) ${contributions} in the ${tagName} release!`;
+  }
 </script>
 
 <svelte:head>
-  <title>@{data.certificate.user.login}'s {data.certificate.to.tagName} contributions</title>
+  <title>{pageTitle}</title>
 </svelte:head>
 
 <MetaTags
   openGraph={{
-    title: `@${data.certificate.user.login}'s ${data.certificate.to.tagName} contributions`,
+    title: `${data.certificate.to.tagName} acmcsuf.com contributions | ${data.certificate.user.name}`,
+    description: makeDescription(data.certificate),
     images: [
       {
-        url: data.certificate.user.picture,
+        url: new URL(data.certificate.user.picture, $page.url).toString(),
         width: 80,
         height: 80,
         alt: `${data.certificate.user.name}'s picture`,
