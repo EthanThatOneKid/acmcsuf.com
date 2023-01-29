7881afc3ae1bc027ae1d3e2616035ef7b01e24eb
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-01-29 15:40:22 -0800

    Added CSS.
    Need to Fixes like the Frank image holder and the text placement sizes along with the unorganized CSS will be changed.

--- src/routes/(site)/pomo/+page.svelte
+++ src/routes/(site)/pomo/+page.svelte
@@ -21,7 +21,7 @@
   let pattern = PomoPattern.fromString($page.params.pattern || defaultPattern);
   let timestamp = new Date();
   let timer: number;
-  let info= pattern.at(timestamp, todayStart);
+  let info = pattern.at(timestamp, todayStart);
 
   $: {
     if (browser) {
@@ -38,113 +38,183 @@
       timestamp = new Date();
     }, 1e3) as unknown as number;
 
-    return () => clearInterval(timer)
+    return () => clearInterval(timer);
   });
+
+  /* For the Menu Part to see the Work on A smaller phone Screen 
+
+ let menu = document.getElementById('menu');
+  let btns = document.querySelectorAll('.btn');
+  if (menu) {
+    menu.addEventListener('click', () => {
+      btns.forEach((btn) => {
+        btn.classList.toggle('show');
+      });
+    });
+  }
+
+  */
 </script>
 
 <svelte:head>
   <title>Pomo | ACM at CSUF</title>
 </svelte:head>
 
-
-
 <Spacing --min="175px" --med="200px" --max="200px" />
- 
-<Block>
-
-  <h1 slot="headline" class="size-xl">🍅 Pomodomo Timer</h1>
- 
-</Block> 
- 
-<section>
-  <div class = 'button-container'>
-    <button id = 'btn'>Work 1</button>
-    <button id = 'btn'>Work 2</button>
-    <button id = 'btn'>Work 3</button>
-    <button id = 'btn'>Work 4</button>
-    <button id = 'btn'>Work 5</button>
-  </div>
-
-  <p class="size-md">
-    {durationFmt(info.remainingMs)}
-  </p>
-
-<div class = 'toggle-container'>
-  <button class = 'toggle-btns start'> Start </button>
-  <button class = 'toggle-btns stop'> Stop </button>
+<div class="title-container">
+  <Block>
+    <h1 slot="headline" class="size-xl">Pomodomo Timer</h1>
+    <h3 slot="text" class="size-xl text">
+      Official at CSUF ACM <img
+        src="src\routes\(site)\pomo\acm-csuf-badge.png"
+        alt="ACM Logo"
+        class="acm"
+      />
+    </h3>
+  </Block>
 </div>
-  
 
-</section>
+<div class="timer-container">
+  <img src="src\routes\(site)\pomo\acm-shark.png" alt="Photo of Frank" class="frank" />
+  <section>
+    <button id="menu" style="display: none">Menu</button>
+    <div class="button-container">
+      <button id="btn" class="b">Work</button>
+      <button id="btn" class="b">Work</button>
+      <button id="btn" class="b">Work</button>
+      <button id="btn" class="b">Work</button>
+      <button id="btn" class="b">Work</button>
+    </div>
 
+    <p class="size-md timer">
+      {durationFmt(info.remainingMs)}
+    </p>
+
+    <h2 class="work-period name">Starting TypeScript work pattern...</h2>
+    <div class="toggle-container">
+      <button class="toggle-btns start"> Start </button>
+      <button class="toggle-btns stop"> Stop </button>
+    </div>
+  </section>
+</div>
 
 <Spacing --min="100px" --med="175px" --max="200px" />
 
-
-
 <Spacing --med="64px" />
-
-
 
 <Spacing --min="40px" --med="95px" --max="120px" />
 
-
 <style lang="scss">
-
-  .size-md {
-    margin: 40px 0px 40px 0px;
-  }
-  .size-xl {
-    font-size: 60px;
-    margin-bottom: 50px;
-    color: var(--acm-white);
-  }
-
-  section {
-    padding: 60px 10px 60px 10px;
-    background-color: var(--acm-blue);
-    box-shadow: 0 0 30px var( --acm-sky);
-   
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 750px;
-    border-radius: 15px;
+  * {
+    margin: 10px;
   }
 
   button {
-    background: none;
-    border: none;
-    font-weight: 600;
     cursor: pointer;
-    font-size: 25px;
   }
 
   button:hover {
-    border-bottom: solid 1px var(--acm-blue);
-    transform: scale(1.1);
-    box-shadow: 0 0 15px var(--acm-gray);
-    border-radius: 50px;
+    transform: scale(1.05);
+  }
+  .acm {
+    max-width: 50px;
+    margin-left: 5px;
+  }
+
+  .frank {
+    max-width: 500px;
+    position: absolute;
+    left: 13%;
+    top: 43%;
+    z-index: -1;
+  }
+
+  .title-container {
+    margin-bottom: 170px;
+  }
+
+  .size-xl {
+    font-size: 90px;
+    margin-bottom: 5px;
+    color: var(--acm-white);
+  }
+
+  @media screen and (max-width: 1000px) {
+    .size-xl {
+      font-size: 65px;
+    }
+
+    #btn {
+      display: none;
+    }
+
+    .timer {
+      font-size: 25px;
+    }
+
+    .frank {
+      top: 58%;
+      left: 10%;
+      max-width: 400px;
+    }
+  }
+
+  section {
+    padding: 30px 90px 90px 90px;
+    background-color: var(--acm-sky);
+    box-shadow: 0 0 12px var(--acm-sky);
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 850px;
+    border-radius: 15px;
+  }
+
+  .work-period {
+    text-align: center;
+    margin: 40px;
+  }
+
+  #btn {
+    background: none;
+    border: none;
+    font-weight: 600;
+    font-size: 30px;
+    padding: 15px;
+    border-bottom: solid 1px var(--acm-light);
+  }
+
+  #btn:hover {
+    background-color: var(--acm-bluer);
   }
 
   section .size-md {
-    font-size: 65px;
-    font-weight: 900;
+    font-size: 95px;
     text-align: center;
+    font-weight: 900;
+    margin: 40px 0px 40px 0px;
+  }
+
+  .timer {
+    background-color: var(--acm-blue);
+    padding: 50px;
+    border-radius: 15px;
+    box-shadow: 0 0 5px rgb(52, 105, 165); /* Can we add this as a color? */
+  }
+
+  .text {
+    font-size: 40px;
   }
 
   .button-container {
-    padding: 20px 1px 50px 1px;
+    padding: 20px 1px 20px 1px;
     display: flex;
     justify-content: space-evenly;
-    flex-wrap: wrap;
-    color:rgba(215, 213, 224, 0.95);
   }
 
   .button-container button {
     padding: 13px;
     margin-top: 20px;
     border-bottom: solid 2px var(--acm-dark);
-    
   }
 
   .toggle-container {
@@ -157,12 +227,10 @@
     background: none;
     color: var(--acm-dark);
     padding: 20px;
-    width: 200px;
-    border-radius: 10px;
-    border: solid 1px rgba(213, 202, 202, 0.593);
+    width: 300px;
     font-size: 40px;
+    border: solid px var(--acm-gray);
     box-shadow: 0 0 8px var(--acm-gray);
-
   }
 
   .toggle-container .start {
