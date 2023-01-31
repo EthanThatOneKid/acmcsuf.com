2de0f61dbaf4865d0a666e6bdb0c9afcd5ffd3f8
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-01-31 01:28:12 -0800

    Add video support
    TODO: Write documentation for going about setting up the GitHub repository environment for the `.github/workflows/update_genuary.yaml` file.

--- src/routes/(site)/genuary/collage.svelte
+++ src/routes/(site)/genuary/collage.svelte
@@ -8,41 +8,82 @@
   <div class="grid-inner-wrapper">
     {#each data as { src, view, alt: givenAlt }, i}
       {@const alt = givenAlt || src}
-      <div>
-        <img
-          {src}
-          class:wide={view === 'wide'}
-          class:tall={view === 'tall'}
-          class:big={view === 'big'}
-          {alt}
-          style:animation-delay={i * 2 + 's'}
-        />
-      </div>
+      {@const video = src.endsWith('.mp4') || src.endsWith('.mov')}
+      <details class="modal artwork-container">
+        <summary>
+          {#if video}
+            <video
+              {src}
+              autoplay
+              loop
+              muted
+              playsinline
+              class="artwork"
+              class:wide={view === 'wide'}
+              class:tall={view === 'tall'}
+              class:big={view === 'big'}
+              style:animation-delay={i * 2 + 's'}
+            />
+          {:else}
+            <img
+              {src}
+              class="artwork"
+              class:wide={view === 'wide'}
+              class:tall={view === 'tall'}
+              class:big={view === 'big'}
+              {alt}
+              style:animation-delay={i * 2 + 's'}
+            />
+          {/if}
+        </summary>
+
+        {#if video}
+          <video {src} class="modal-artwork" autoplay loop muted playsinline />
+        {:else}
+          <img class="modal-artwork" {src} {alt} />
+        {/if}
+      </details>
     {/each}
   </div>
 </div>
 
 <!-- Reference: https://codepen.io/iamsaief/pen/jObaoKo -->
 <style lang="scss">
-  .grid-outer-wrapper {
-    padding: 15px;
+  .modal {
+    max-width: 100%;
+    height: auto;
   }
 
-  img {
+  .modal summary {
+    cursor: pointer;
+    margin: 0;
     max-width: 100%;
     height: auto;
     vertical-align: middle;
     display: inline-block;
   }
 
+  .modal summary:hover {
+    border: 1px solid black;
+  }
+
+  .grid-outer-wrapper {
+    padding: 15px;
+  }
+
+  .artwork-container {
+    max-width: 100%;
+    height: auto;
+  }
+
   /* Main CSS */
-  .grid-inner-wrapper > div {
+  .grid-inner-wrapper > .artwork-container {
     display: flex;
     justify-content: center;
     align-items: center;
   }
 
-  .grid-inner-wrapper > div > img {
+  .grid-inner-wrapper > .artwork-container > .artwork {
     width: 100%;
     height: 100%;
     object-fit: cover;
