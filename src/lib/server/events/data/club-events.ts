75a29496c7c489ca84824b17c34eb00bdd40107f
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2023-03-10 00:44:41 -0800

    Create sample test data for acmcsuf.com/events
    Co-Authored-By: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>

--- src/lib/server/events/data/club-events.ts
+++ src/lib/server/events/data/club-events.ts
@@ -1,4 +1,4 @@
 import type { ClubEvent } from '$lib/public/events/event';
-import SAMPLE_EVENTS_JSON from './sample-events.json';
+import SAMPLE_EVENTS_JSON from './club-events.json';
 
 export const SAMPLE_EVENTS: ClubEvent[] = [...SAMPLE_EVENTS_JSON];
