9ee9537600c72941ae1f0bf00a9b00c2662ec50f
jiink <jacob.j.carlson03@gmail.com>
2023-03-28 13:45:29 -0700

    Full paths for test so job can use it
    Co-Authored-By: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>

--- scripts/transform-genuary/test.js
+++ scripts/transform-genuary/test.js
@@ -6,16 +6,16 @@ const ALLOWED_MEDIA_DOMAINS = ["cdn.discordapp.com", "i.imgur.com"];
 main();
 
 async function main() {
-  const filepath = "./in.json";
+  const filepath = "./scripts/transform-genuary/in.json";
   const data = JSON.parse(await fs.readFile(filepath));
 
-  const expectedFilepath = "./out.json";
-  const expectedData = fs.readFile(expectedFilepath);
+  const expectedFilepath = "./scripts/transform-genuary/out.json";
+  const expectedData = await fs.readFile(expectedFilepath);
 
   const media = getMediaFromPins(data, 2023);
   const outputLog = JSON.stringify(media, null, 2);
   if (expectedData != outputLog) {
-    console.log("We have a prbolem");
+    console.log("We have a problem");
     process.exit(1);
   }
 }
