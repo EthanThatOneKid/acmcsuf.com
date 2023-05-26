f52f88d6320d4eb72999fcf83910c8fe1e1a3153
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-05-25 20:47:05 -0700

    Add `/boardify` page to crop board member images (#846)
    * Create +page.svelte
    
    * push image cropper
    
    * Progress (incomplete)
    
    * `npm i cropperjs`
    
    * Move `/boardify` to `(site)` group from `(standalone)` group
    
    * Add `/boardify` page instructions
    
    * Fix Boardify lint errors
    
    * Update Boardify instructions
    
    * Update `cropperjs` module
    
    * Run `npm run all`
    
    ---------
    
    Co-authored-by: Angel S <santoyox714@gmail.com>

---
+++ src/lib/components/board-member/board-member.svelte
@@ -0,0 +1,25 @@
+<!--
+TODO: Show complete board member preview.
+- See: https://github.com/EthanThatOneKid/acmcsuf.com/blob/acac11c14edad5445d5e59dfb92afc36b96544e9/src/routes/(site)/about/officer-profile.svelte
+- See: https://codepen.io/alancortez1337/pen/NWzKqKE?editors=1100
+-->
+<script lang="ts">
+  import BoardPicture from './board-picture.svelte';
+
+  export let src: string;
+  export let teamColor: string;
+</script>
+
+<div class="board-member__container">
+  <BoardPicture {src} color={teamColor} />
+</div>
+
+<style lang="scss">
+  input,
+  select {
+    display: inline-block;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
+</style>
