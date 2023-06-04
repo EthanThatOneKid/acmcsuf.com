9934683285845f42fdb7672c178e473eb488d052
Louiza <louiz.denane02@gmail.com>
2023-06-04 10:08:27 +0200

    fix inconsistent variable name (Url) (#866)
    * fix inconsistent variable name (Url)
    
    * avatarURL --> avatarUrl
    
    * avatarURL->avatarUrl
    
    * update all Url->URL variables
    
    * fix format errors

--- src/lib/public/blog/urls.ts
+++ src/lib/public/blog/urls.ts
@@ -1,15 +1,15 @@
 import { ALL, queryFromLabels } from './utils';
 
-export function makeBlogPostsJsonUrl(labels: string[]): string {
+export function makeBlogPostsPageDataURL(labels: string[]): string {
   const query = queryFromLabels(labels);
   return `/blog/${query}.json`;
 }
 
-export function makeBlogPostsPageUrl(labels: string[]): string {
+export function makeBlogPostsPageURL(labels: string[]): string {
   const query = queryFromLabels(labels);
   return query === ALL ? '/blog' : `/blog?l=${query}`;
 }
 
-export function makeBlogPostPageUrl(id: number): string {
+export function makeBlogPostPageURL(id: number): string {
   return `/blog/${id}`;
 }
