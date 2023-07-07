9156a44d764941ed33b801a607bfcd4285a30edc
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-07-07 15:03:30 -0700

    Run `npm create svelte@latest .`
    TODO: Consolidate old config files with new config files.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,30 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  plugins: ['svelte3', '@typescript-eslint'],
-  env: { browser: true, node: true },
-  ignorePatterns: ['*.cjs'],
-  overrides: [
-    { files: ['*.svelte'], processor: 'svelte3/svelte3' },
-    { files: ['*.js'], rules: { '@typescript-eslint/explicit-module-boundary-types': 0 } },
-  ],
-  settings: {
-    'svelte3/typescript': () => require('typescript'),
-    'svelte3/ignore-styles': () => true, // ignore svelte styles since we use scss
-  },
-  rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'func-style': ['error', 'declaration', { allowArrowFunctions: false }],
-    'prefer-arrow-callback': 'error',
-    'no-nested-ternary': ['error'],
-    'comma-dangle': 'off',
-    '@typescript-eslint/comma-dangle': ['error', 'only-multiline'],
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-  },
+	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	]
 };
