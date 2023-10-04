4faea2b9955845e97ef53b6c2a777cf42e640108
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-10-03 21:37:05 -0700

    Clear Playwright tests (#922)
    * Update check_website.yaml
    
    * Drop `@playwright/test` tool
    
    Resolves <https://github.com/EthanThatOneKid/acmcsuf.com/pull/884#pullrequestreview-1555545355>.
    
    ### Technique
    
    run `for /r %a in ("page.test.ts") do del "%a"`.

--- scripts/check_site_changes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Name: check_site_changes.sh
-# Description: Exits with code 1 (new build needed) or code 0
-# Usage: bash scripts/check_site_changes.sh
-# Tracker: https://etok.codes/acmcsuf.com/issues/198
-# Requires: Git (https://git-scm.com/)
-
-[[ "$(git status --porcelain -- src/ static/ package*.json svelte.config.js)" ]] && exit 1
