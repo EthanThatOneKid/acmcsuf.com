8231c0ad25b1cf6002d533dce51512a11fe7ee89
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-10-03 21:15:38 -0700

    Drop `@playwright/test` tool
    Resolves <https://github.com/EthanThatOneKid/acmcsuf.com/pull/884#pullrequestreview-1555545355>.
    
    ### Technique
    
    run `for /r %a in ("page.test.ts") do del "%a"`.

--- src/routes/(site)/privacy/page.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test.describe.configure({ mode: 'parallel' });
-
-test('privacy page has expected h1', async ({ page }) => {
-  await page.goto('/privacy');
-  expect(await page.textContent('h1')).toBe('Privacy Policy for ACM at CSUF');
-});
--- src/routes/(site)/wave2/page.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test.describe.configure({ mode: 'parallel' });
-
-test('wave2 page has expected h1', async ({ page }) => {
-  await page.goto('/wave2');
-  expect(await page.textContent('h1')).toBe('Wave 2 board applications');
-});
