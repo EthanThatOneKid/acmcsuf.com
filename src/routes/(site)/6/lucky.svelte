1ac32fc26c49ec41f6cc468d62492ec7369a6561
Brian Milian <106508985+brian-250@users.noreply.github.com>
2023-10-12 16:57:28 -0700

    WIP
    Create lucky component

---
+++ src/routes/(site)/6/lucky.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import AcmButton from '$lib/components/button/button.svelte';
+</script>
+
+<section class="hero-container">
+  <div class="hero-inner-container">
+    <div class="hero-text">
+      <h1 class="brand-header size-xl">
+        I'm feeling lucky!
+      </h1>
+      <div class="join-button">
+        <AcmButton text="Join today!" link="/discord" redirect={true} />
+      </div>
+    </div>
+
+    <img
+      src="/assets/png/capy-lucky.png"
+      alt="Chip the Capybara surrounded by all ACM Team badges"
+    />
+  </div>
+</section>
