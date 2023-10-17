6fcaf0a94138908a8dcdb25792deaab79a4b7ad3
Brian Milian <106508985+brian-250@users.noreply.github.com>
2023-10-16 19:42:18 -0700

    Update CSS in lucky.svelte

--- src/routes/(site)/6/lucky.svelte
+++ src/routes/(site)/6/lucky.svelte
@@ -2,13 +2,11 @@
   import AcmButton from '$lib/components/button/button.svelte';
 </script>
 
-<section class="hero-container">
-  <div class="hero-inner-container">
-    <div class="hero-text">
-      <h1 class="brand-header size-xl">
-        I'm feeling lucky!
-      </h1>
-      <div class="join-button">
+<section class="lucky-container">
+  <div class="lucky-inner-container">
+    <div class="lucky-text">
+      <h1 class="brand-header size-xl">I'm feeling lucky!</h1>
+      <div class="lucky-button">
         <AcmButton text="Join today!" link="/discord" redirect={true} />
       </div>
     </div>
@@ -19,3 +17,29 @@
     />
   </div>
 </section>
+
+<style>
+  section {
+    display: grid;
+  }
+  section .lucky-inner-container {
+    display: grid;
+    grid-template-columns: 1fr;
+    align-items: center;
+    gap: 3em;
+  }
+  section .lucky-inner-container .lucky-text {
+    display: grid;
+    align-items: center;
+    text-align: center;
+    gap: 3em;
+  }
+  section .lucky-inner-container .lucky-text .lucky-button {
+    display: grid;
+    justify-self: center;
+  }
+  section .lucky-inner-container img {
+    max-width: clamp(20rem, 17.342rem + 10.13vw, 30rem);
+    justify-self: center;
+  }
+</style>
