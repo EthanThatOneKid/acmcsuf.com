6324b30c0a0df4fc784a0bac41bdbe6d99e2d795
Sama Ahmed <26samahmed@csu.fullerton.edu>
2023-11-03 13:44:27 -0700

    Update testimony.svelte
    Finish Desktop CSS

--- src/routes/(site)/6/testimony.svelte
+++ src/routes/(site)/6/testimony.svelte
@@ -5,8 +5,8 @@
 <section class="testimony-container">
   <div class="testimony-inner-container">
     <h1 class="brand-header size-xl">Hear what our community has to say</h1>
-    <div class="testimony-text">
-      <div class="testimony-photo">
+    <div class="testimony-text1">
+      <div class="testimony-photo1">
         <BoardPicture src="/assets/authors/placeholder.webp" color="var(acm-blue)" />
       </div>
       <blockquote>
@@ -18,8 +18,8 @@
       </blockquote>
     </div>
 
-    <div class="testimony-text">
-      <div class="testimony-photo">
+    <div class="testimony-text2">
+      <div class="testimony-photo2">
         <BoardPicture src="/assets/authors/placeholder.webp" color="var(acm-blue)" />
       </div>
       <blockquote>
@@ -49,20 +49,86 @@
     text-align: center;
     margin: 0.3em;
   }
-  section .testimony-inner-container .testimony-text {
+  section .testimony-inner-container .testimony-text1,
+  .testimony-text2 {
     display: grid;
     align-items: center;
   }
 
-  section .testimony-inner-container .testimony-text p {
+  section .testimony-inner-container .testimony-text1 p {
     margin-left: 2em;
     margin-right: 2em;
   }
 
-  section .testimony-inner-container .testimony-photo {
+  section .testimony-inner-container .testimony-text2 p {
+    margin-left: 2em;
+    margin-right: 2em;
+  }
+
+  section .testimony-inner-container .testimony-photo1 {
     display: grid;
     align-items: center;
     justify-self: center;
     margin: 2em;
+  }
+
+  section .testimony-inner-container .testimony-photo2 {
+    display: grid;
+    align-items: center;
+    justify-self: center;
+    margin: 2em;
+  }
+
+  @media screen and (min-width: 768px) {
+    section {
+      display: grid;
+      max-width: 1280px;
+      margin: 0 auto;
+    }
+    section .testimony-inner-container {
+      display: grid;
+      grid-template-columns: 1fr;
+      align-items: center;
+    }
+
+    section .testimony-inner-container h1 {
+      display: grid;
+      align-items: center;
+      text-align: center;
+      margin: 0.3em;
+    }
+    section .testimony-inner-container .testimony-text1,
+    .testimony-text2 {
+      display: grid;
+      align-items: center;
+    }
+
+    section .testimony-inner-container .testimony-text1 p {
+      margin-left: 18em;
+      margin-right: 12em;
+      margin-top: -8.5em;
+    }
+
+    section .testimony-inner-container .testimony-text2 p {
+      margin-left: 11em;
+      margin-right: 19em;
+      margin-top: -8.5em;
+    }
+
+    section .testimony-inner-container .testimony-photo1 {
+      display: grid;
+      align-items: center;
+      justify-self: left;
+      margin: 2em;
+      margin-left: 7em;
+    }
+
+    section .testimony-inner-container .testimony-photo2 {
+      display: grid;
+      align-items: center;
+      justify-self: right;
+      margin: 2em;
+      margin-right: 9em;
+    }
   }
 </style>
