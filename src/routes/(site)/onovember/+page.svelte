6c8198bff52b377d5115b49082d9833c4e3d35f2
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-06 15:33:01 -0800

    handy dandy `display: grid` to the rescue

--- src/routes/(site)/onovember/+page.svelte
+++ src/routes/(site)/onovember/+page.svelte
@@ -40,9 +40,9 @@
       </h2>
 
       <ol>
-        {#each onovember.winners as { usernames, submissionCount } (name)}
+        {#each onovember.winners as { usernames, submissionCount }}
           <li>
-            {#each usernames as username, i (i)}
+            {#each usernames as username, i}
               {#if i > 0}, {/if}
               <a href={`https://leetcode.com/${username}`} title={username}>{username}</a>
             {/each}
@@ -52,16 +52,24 @@
       </ol>
 
       <div class="onovember-section">
-        {#each onovember.calendar as { dayOfMonth, submissionCount, submissionsText, question }, i (i)}
+        {#each onovember.calendar as { dayOfMonth, submissionCount, submissionsText, question }}
           {#if question}
-            <div class="onovember-cell" style:--submission-count={submissionCount}>
-              <a href={question.url} title={submissionsText}>Nov {dayOfMonth}<br /></a><code
-                >{submissionCount}</code
-              > submissions
+            <div
+              class="onovember-cell"
+              title={submissionsText}
+              style:--submission-count={submissionCount}
+            >
+              Nov {dayOfMonth}
+              <hr />
+              Answered by <code>{submissionCount}</code>
+              <hr />
+              <a href={question.url}>{question.title}</a>
             </div>
           {:else}
             <div class="onovember-cell" style:--submission-count="0">
-              Nov {dayOfMonth}<br /><code>0</code> submissions
+              Nov {dayOfMonth}
+              <hr />
+              Answered by <code>0</code>
             </div>
           {/if}
         {/each}
@@ -81,6 +89,7 @@
   h1 {
     text-align: center;
     font-size: 24px;
+    padding: 2em;
   }
 
   h2 {
@@ -97,6 +106,13 @@
     margin-left: 5px;
   }
 
+  .onovember-section {
+    --onovember-width: 150px;
+
+    display: grid;
+    grid: auto-flow / repeat(auto-fill, minmax(var(--onovember-width), 1fr));
+  }
+
   .onovember-cell {
     background-color: hsl(24, 100%, calc(50% - var(--submission-count, 0) * 5%));
     padding: 1em;
@@ -104,10 +120,11 @@
     border-radius: 5px;
     display: inline-block;
     text-align: center;
+    max-width: var(--onovember-width);
   }
 
   ol {
-    margin-left: 0px;
+    margin: 0 2em 2em 2em;
   }
 
   a {
@@ -116,5 +133,9 @@
 
   a:hover {
     text-decoration: underline;
+  }
+
+  code {
+    font-size: 16px;
   }
 </style>
