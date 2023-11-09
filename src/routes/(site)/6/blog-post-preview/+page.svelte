80d76af3242f40f86b0a67e719ad195aa099f9c2
mattulau <mattlau7890@gmail.com>
2023-11-08 20:27:03 -0800

    start blog post preview component

---
+++ src/routes/(site)/6/blog-post-preview/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import BlogPostPreview from './blog-post-preview.svelte';
+  import BLOG_POST from '../data.json';
+  import Spacing from '$lib/public/legacy/spacing.svelte';
+</script>
+
+<Spacing --min="175px" --med="200px" --max="200px" />
+<BlogPostPreview data={BLOG_POST[0]} />
