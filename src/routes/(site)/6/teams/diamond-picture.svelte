33f7111981ee342a31868a68bbbd4ad7bda9e8d2
Evan Jimenez <evanmji1123@gmail.com>
2023-11-21 21:39:15 -0800

    Acm logo animation (#975)
    * Added General gif animation test
    
    * Put General logo into diamond shape, is awesome
    
    * Formatted or something
    
    * Created diamond-picture.svelte
    
    * Added reduced motion option for General picture
    
    * It formatted.. again...

---
+++ src/routes/(site)/6/teams/diamond-picture.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  export let src: string;
+  export let reducedMotionSrc: string;
+  export let altSrc: string;
+</script>
+
+<img class="reduced-motion" src={reducedMotionSrc} alt={altSrc} />
+
+<svg
+  class="non-reduced-motion"
+  viewBox="-50 -50 1200 1200"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <defs>
+    <clipPath id="shape2">
+      <path
+        d="M53.093 646.116C-17.6508 575.372 -17.6508 460.674 53.093 389.93L389.93 53.0929C460.674 -17.6509 575.372 -17.6509 646.116 53.093L982.954 389.93C1053.7 460.674 1053.7 575.372 982.954 646.116L646.116 982.953C575.372 1053.7 460.674 1053.7 389.93 982.953L53.093 646.116Z"
+      />
+    </clipPath>
+  </defs>
+
+  <!-- Create a path for the border -->
+  <g transform="translate(-50, -50) scale(1.1)">
+    <path
+      d="M53.093 646.116C-17.6508 575.372 -17.6508 460.674 53.093 389.93L389.93 53.0929C460.674 -17.6509 575.372 -17.6509 646.116 53.093L982.954 389.93C1053.7 460.674 1053.7 575.372 982.954 646.116L646.116 982.953C575.372 1053.7 460.674 1053.7 389.93 982.953L53.093 646.116Z"
+      fill="rgba(var(--color, var(--acm-general-rgb))"
+    />
+  </g>
+
+  <image clip-path="url(#shape2)" xlink:href={src} />
+</svg>
+
+<style lang="scss">
+  svg,
+  img {
+    max-width: clamp(20rem, 17.342rem + 10.13vw, 30rem);
+    justify-self: center;
+  }
+  .reduced-motion {
+    display: none;
+  }
+  @media (prefers-reduced-motion: reduce) {
+    .reduced-motion {
+      display: initial;
+    }
+    .non-reduced-motion {
+      display: none;
+    }
+  }
+  image {
+    width: 87%;
+    height: 87%;
+  }
+</style>
--- src/routes/(site)/6/teams/team-section.svelte
+++ src/routes/(site)/6/teams/team-section.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { Team, Term } from '$lib/public/board/types';
   import { TextAlignment } from '$lib/public/text-alignment/text-alignment';
+  import DiamondPicture from './diamond-picture.svelte';
   import Members from './members.svelte';
 
   export let textAlign: TextAlignment = TextAlignment.RIGHT;
@@ -18,7 +19,16 @@
       class:dev-animation={info.id === 'dev'}
       class:oss-animation={info.id === 'oss'}
     >
-      <img src={info.logoSrc} alt={`${info.title} Team Logo`} />
+      {#if info.id === 'general'}
+        <DiamondPicture
+          src="https://cdn.discordapp.com/icons/710225099923521558/a_f72bf9caa196d84a44ff40cdfd3f8d9a.gif?size=1024"
+          reducedMotionSrc={info.logoSrc}
+          altSrc="General Picture"
+        />
+      {:else}
+        <img src={info.logoSrc} alt={`${info.title} Team Logo`} />
+      {/if}
+
       <div class="team-description">
         <h2>
           <span class="headers size-lg">
@@ -123,8 +133,11 @@
   }
 
   @media (prefers-reduced-motion: reduce) {
+    /* Stops team logo animations when reduced animation is on :) */
+    .marketing-animation img,
+    .design-animation img,
     .dev-animation img,
-    .marketing-animation img {
+    .oss-animation img {
       animation: none;
     }
   }
