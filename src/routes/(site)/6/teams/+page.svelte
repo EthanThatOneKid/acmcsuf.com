d965a17233d5053252658debfd02c05accd9615f
Evan Jimenez <evanmji1123@csu.fullerton.edu>
2023-11-21 19:55:26 -0800

    Design component (#974)
    * Added Design team with placeholder text, along with logo flip animation
    
    * changed design text a bit
    
    * npm run all changed formatting
    
    * Fixed tier in Spring 2022
    
    * Fixed design team text color

--- src/routes/(site)/6/teams/+page.svelte
+++ src/routes/(site)/6/teams/+page.svelte
@@ -51,6 +51,12 @@
 <!-- TODO: Algo -->
 
 <!-- TODO: Design -->
+<TeamSection info={TEAMS.design} textAlign={TextAlignment.RIGHT} term={VISIBLE_TERMS[$termIndex]}>
+  <p slot="content" class="size-md">
+    The <span class="brand-pink brand-em">design</span> team is awesome :capycool:
+    <span class="brand-pink brand-em">Design</span> does more than just web dev :surreallick:
+  </p>
+</TeamSection>
 
 <TeamSection info={TEAMS.dev} textAlign={TextAlignment.RIGHT} term={VISIBLE_TERMS[$termIndex]}>
   <p slot="content" class="size-md">
