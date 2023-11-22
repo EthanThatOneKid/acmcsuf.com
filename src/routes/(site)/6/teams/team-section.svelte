6bb469dab0c316cd0a5ee575a72f033884baa46b
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-21 20:41:02 -0800

    Update team-section.svelte

--- src/routes/(site)/6/teams/team-section.svelte
+++ src/routes/(site)/6/teams/team-section.svelte
@@ -114,11 +114,38 @@
   }
 
   @keyframes tilt {
-    from {
-      transform: rotate(-15deg);
+    0% {
+      transform: translate(1px, 1px) rotate(-15deg);
     }
-    to {
-      transform: rotate(15deg);
+    10% {
+      transform: translate(-1px, -2px) rotate(-12deg);
+    }
+    20% {
+      transform: translate(-3px, 0px) rotate(-9deg);
+    }
+    30% {
+      transform: translate(3px, 2px) rotate(-6deg);
+    }
+    40% {
+      transform: translate(1px, -1px) rotate(-3deg);
+    }
+    50% {
+      transform: translate(-1px, 2px) rotate(0deg);
+    }
+    60% {
+      transform: translate(-3px, 1px) rotate(3deg);
+    }
+    70% {
+      transform: translate(3px, 1px) rotate(6deg);
+    }
+    80% {
+      transform: translate(-1px, -1px) rotate(9deg);
+    }
+    90% {
+      transform: translate(1px, 2px) rotate(12deg);
+    }
+    100% {
+      transform: translate(1px, -2px) rotate(15deg);
     }
   }
 
