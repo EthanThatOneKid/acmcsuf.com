bde9c8e1b486e9e00ab43d80cf6fee4db49f1907
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-26 22:38:17 -0800

    run `npm run format`

--- src/routes/(site)/6/teams/+page.svelte
+++ src/routes/(site)/6/teams/+page.svelte
@@ -72,10 +72,10 @@
 <!-- TODO: AI -->
 <TeamSection info={TEAMS.ai} textAlign={TextAlignment.RIGHT} term={VISIBLE_TERMS[$termIndex]}>
   <p slot="content" class="size-md">
-    The <span class="brand-emerald brand-em">artificial intelligence</span> is dedicated to providing
-    accessible information about artificial intelligence and machine learning to all.
-    <span class="brand-emerald brand-em">AI</span> focuses on fun projects geared towards 
-    beginners in the field.
+    The <span class="brand-emerald brand-em">artificial intelligence</span> is dedicated to
+    providing accessible information about artificial intelligence and machine learning to all.
+    <span class="brand-emerald brand-em">AI</span> focuses on fun projects geared towards beginners in
+    the field.
   </p>
 </TeamSection>
 
