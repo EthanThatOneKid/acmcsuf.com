f273b8b88e8592176c159905c552cf345cda6b8a
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-12-04 20:50:42 -0800

    pop off 💅

--- src/routes/(site)/[x+40][username]/+page.ts
+++ src/routes/(site)/[x+40][username]/+page.ts
@@ -1,10 +1,10 @@
 import type { PageLoadEvent } from './$types';
-import type { CertificatePageData } from '$lib/public/certificates';
-import { LATEST, makeCertificatePageDataURL } from '$lib/public/certificates/urls';
+import type { ReleaseCertificatePageData } from '$lib/public/certificates';
+import { LATEST, makeReleaseCertificatePageDataURL } from '$lib/public/certificates/urls';
 
 export async function load({ fetch, params, url }: PageLoadEvent) {
   const release = url.searchParams.get('release') ?? LATEST;
-  const response = await fetch(makeCertificatePageDataURL(params.username, release));
-  const data: CertificatePageData = await response.json();
+  const response = await fetch(makeReleaseCertificatePageDataURL(params.username, release));
+  const data: ReleaseCertificatePageData = await response.json();
   return data;
 }
