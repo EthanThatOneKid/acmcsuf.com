1fd79112f4cad4f28866915df2462db414f9e171
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-23 13:22:54 -0800

    Fixed overflow on teams component (landing page)

--- src/routes/(site)/teams.svelte
+++ src/routes/(site)/teams.svelte
@@ -121,10 +121,21 @@
     }
 
     section .hero-inner-container {
+      grid-template-columns: 1fr;
       gap: 2em;
       margin: 1em;
     }
 
+    section .hero-inner-container .hero-text {
+      text-align: start;
+    }
+
+    section .hero-inner-container .hero-text .discover-button {
+      justify-self: center;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
     section .hero-inner-container .team-row1-container {
       display: grid;
       grid-template-columns: 1fr 1fr 1fr;
@@ -139,14 +150,6 @@
       justify-content: center;
       width: 70%;
       gap: 2em;
-    }
-
-    section .hero-inner-container .hero-text {
-      text-align: start;
-    }
-
-    section .hero-inner-container .hero-text .discover-button {
-      justify-self: center;
     }
   }
 </style>
