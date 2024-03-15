0256dcf56215bfd24fd62cdf9fad3dd9f4465fd7
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-03-15 12:54:00 -0700

    add drive lib
    Co-Authored-By: Karnikaa Velumani <60043611+karnikaavelumani@users.noreply.github.com>

---
+++ src/routes/(site)/gallery/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<details>
+  <summary>DEBUG</summary>
+  <pre><code>{JSON.stringify(data, null, 2)}</code></pre>
+</details>
+
+<!-- TODO: Render gallery page. -->
+
+<section class="gallery">
+  {#each data.gallery as item (item.id)}
+    <iframe
+      src="https://drive.google.com/file/d/{item.id}/preview"
+      title="Embedded Document"
+      width="640"
+      height="480"
+      allow="autoplay"
+    />
+  {/each}
+</section>
+
+<style>
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
