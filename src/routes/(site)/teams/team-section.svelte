1e2a848c46f6d08ccd885a1d2d63517a5f32dfa5
pmani-250 <24mpatil@csu.fullerton.edu>
2024-04-08 20:25:32 -0700

    fix nodebuds animation on mobile

--- src/routes/(site)/teams/team-section.svelte
+++ src/routes/(site)/teams/team-section.svelte
@@ -275,7 +275,7 @@
     animation-duration: 1.8s;
     animation-name: slide;
     animation-iteration-count: infinite;
-    animation-direction: alternate;
+    animation-direction: alternate; 
     position: relative;
   }
 
@@ -286,7 +286,7 @@
     }
 
     100% {
-      left: 15px;
+      left: 60px;
       top: 0px;
     }
   }
