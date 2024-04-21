d4541bbbe1bcc08e6998d1f203b18f2703f41ac7
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-04-21 15:58:22 -0700

    Update Sitemap Page Design (#1069)
    * create site map page
    
    * add sitemap
    
    * WIP
    
    * Sitemap header styles added
    
    * Small changes
    
    * Update UI for Sitemap Page
    
    * Fix formatting.
    
    ---------
    
    Co-authored-by: Tyler Lui <tylerlui@gmail.com>
    Co-authored-by: Tyler <tyler@Pascals-MacBook-Pro.local>
    Co-authored-by: karnikaavelumani <60043611+karnikaavelumani@users.noreply.github.com>

---
+++ src/routes/(site)/sitemap/+page.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+  import Spacing from '$lib/public/legacy/spacing.svelte';
+  import SITEMAP from './data';
+</script>
+
+<Spacing --min="175px" --med="200px" --max="200px" />
+
+<svelte:head>
+  <title>SiteMap | ACM at CSUF</title>
+</svelte:head>
+
+<section class="sitemap-header">
+  <img src="/assets/sitemap-logo.svg" alt="Sitemap icon in diamond" />
+
+  <div>
+    <h1 class="size-xxl">SITEMAP</h1>
+    <h2 class="size-md">Check out all our pages!</h2>
+  </div>
+</section>
+
+<section class="sitemap-body">
+  <div class="column">
+    {#each Object.entries(SITEMAP) as [category, items]}
+      <div class="category">
+        <h2>{category}</h2>
+        <ul>
+          {#each items as item}
+            <li>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    {/each}
+  </div>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin: 0 24px;
+  }
+
+  section div h1 {
+    font-weight: 550;
+    height: 95px;
+    line-height: 100px;
+  }
+
+  section div h2 {
+    font-weight: 600;
+    margin-left: 5px;
+  }
+
+  section img {
+    max-width: 200px;
+    width: 100%;
+    height: auto;
+    margin-bottom: 8px;
+    margin-right: 2vw;
+  }
+
+  section div li {
+    margin-left: 30px;
+    font-size: 20px;
+  }
+
+  section div li a {
+    text-decoration: none;
+  }
+
+  .sitemap-body {
+    padding: 30px;
+  }
+
+  .column {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  .category {
+    margin: 10px 50px;
+  }
+
+  @media (max-width: 600px) {
+    .sitemap-header {
+      flex-direction: column;
+    }
+
+    .sitemap-header div h2 {
+      text-align: center;
+    }
+
+    section div li {
+      margin-left: 30px;
+    }
+
+    .column {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  @media (min-width: 601px) and (max-width: 900px) {
+    .column {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+</style>
---
+++ src/routes/(site)/sitemap/data.ts
@@ -0,0 +1,127 @@
+import type { SiteMap } from '$lib/public/sitemap';
+
+const siteMap: SiteMap = {
+  'Main pages': [
+    {
+      id: 'homepage',
+      title: 'Home',
+      link: '',
+    },
+    {
+      id: 'eventspage',
+      title: 'Events',
+      link: 'events',
+    },
+    {
+      id: 'teamspage',
+      title: 'Teams',
+      link: 'teams',
+    },
+    {
+      id: 'blogpage',
+      title: 'Blog',
+      link: 'blog',
+    },
+  ],
+  'Board Use': [
+    {
+      id: 'boardifypage',
+      title: 'Boardify',
+      link: 'boardify',
+    },
+    {
+      id: 'colorspage',
+      title: 'Colors',
+      link: 'colors',
+    },
+    {
+      id: 'shorterpage',
+      title: 'Shorter',
+      link: 'shorter',
+    },
+    {
+      id: 'svgpages',
+      title: 'svg',
+      link: 'discord.svg',
+    },
+    {
+      id: 'tempapppage',
+      title: 'Temp-apps',
+      link: 'temp-apps',
+    },
+  ],
+  'Showcase Pages': [
+    {
+      id: 'gallery',
+      title: 'Gallery',
+      link: 'gallery',
+    },
+    {
+      id: 'genuarypage',
+      title: 'Genuary',
+      link: 'genuary',
+    },
+    {
+      id: 'onovemberpage',
+      title: 'Onovember',
+      link: 'onovember',
+    },
+  ],
+  'Productivity pages': [
+    {
+      id: 'lcdailiespage',
+      title: 'LC-dailies',
+      link: 'lc-dailies',
+    },
+    {
+      id: 'pomopage',
+      title: 'Pomo',
+      link: 'pomo',
+    },
+  ],
+  'Contributing Pages': [
+    {
+      id: '1stpage',
+      title: '1st',
+      link: '1st',
+    },
+    {
+      id: 'usernamepage',
+      title: '@[username]',
+      link: '@ethanthatonekid',
+    },
+  ],
+  Miscellaneous: [
+    {
+      id: 'feedxmlpage',
+      title: 'Feed.xml',
+      link: 'feed.xml',
+    },
+    {
+      id: 'quizpage',
+      title: 'Quiz',
+      link: 'quiz',
+    },
+    {
+      id: 'randompage',
+      title: 'Random',
+      link: 'random',
+    },
+  ],
+  Outdated: [
+    {
+      id: 'nodebudspage',
+      title: 'Nodebuds',
+      link: 'nodebuds',
+    },
+  ],
+  Policies: [
+    {
+      id: 'privacypage',
+      title: 'Privacy',
+      link: 'privacy',
+    },
+  ],
+};
+
+export default siteMap;
