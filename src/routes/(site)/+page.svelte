5cc0bd5317623b3fdc823a6e1fd05aed9b8c06cf
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-05-30 18:46:52 -0700

    Update +page.svelte

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -9,6 +9,35 @@
 
 <svelte:head>
   <title>ACM at CSUF</title>
+
+  <!-- Pasted example:
+  https://developers.google.com/search/docs/appearance/structured-data/organization#example -->
+  <!-- Pasted example:
+  https://developers.google.com/search/docs/appearance/structured-data/organization#example -->
+  <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "image": "https://www.example.com/example_image.jpg",
+      "url": "https://www.example.com",
+      "sameAs": ["https://example.net/profile/example1234", "https://example.org/example1234"],
+      "logo": "https://www.example.com/images/logo.png",
+      "name": "Example Corporation",
+      "description": "The example corporation is well-known for producing high-quality widgets",
+      "email": "contact@example.com",
+      "telephone": "+47-99-999-9999",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Rue Improbable 99",
+        "addressLocality": "Paris",
+        "addressCountry": "FR",
+        "addressRegion": "Ile-de-France",
+        "postalCode": "75001"
+      },
+      "vatID": "FR12345678901",
+      "iso6523Code": "0199:724500PMK2A2M1SQQ228"
+    }
+  </script>
 </svelte:head>
 
 <div class="sections-container">
