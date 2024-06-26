f30acbd42e1451723b7a6601a9e9f74f5ad5093e
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-25 23:17:29 -0700

    Update +page.svelte

---
+++ src/routes/(site)/portfolios/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Spacing from '$lib/public/legacy/spacing.svelte';
+  import portfolios from './acm-portfolio-sp2024.json';
+</script>
+
+<svelte:head>
+  <title>Portfolios | ACM at CSUF</title>
+</svelte:head>
+
+<Spacing --min="175px" --med="200px" --max="200px" />
+
+<ul>
+  {#each portfolios as portfolio}
+    <li>
+      <a href={portfolio.url}>{portfolio.title}</a>
+    </li>
+  {/each}
+</ul>
