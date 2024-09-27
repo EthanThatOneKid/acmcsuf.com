78c7571562b8cfe9a06fd8617aa6ebf867e76b1d
Jeremiah <jeremherring@gmail.com>
2024-09-26 18:30:52 -0700

    add footer and navbar to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,19 +1,58 @@
 <script lang="ts">
   import { page } from '$app/stores';
-  import { onMount } from 'svelte';
+  import Bar from '$lib/components/nav/bar.svelte';
+  import Footer from '$lib/components/footer/footer.svelte';
+  import { onMount, onDestroy } from 'svelte';
+  import { AcmTheme, theme } from '$lib/public/legacy/theme';
   import ErrorGame from '$lib/components/error-game/game.svelte';
-  let showGame = false;
+
+  function changeTheme(event: MediaQueryListEvent) {
+    theme.set(event.matches ? AcmTheme.Dark : AcmTheme.Light);
+  }
+
+  function preventKeyScroll(event: KeyboardEvent) {
+    // Prevent spacebar and down arrow from scrolling (interferes with game)
+    if (event.code === 'Space' || event.code === 'ArrowDown') {
+      event.preventDefault();
+    }
+  }
 
   onMount(() => {
-    // Only show the game component on the client
-    showGame = true;
+    theme.init();
+
+    const mediaList = window.matchMedia('(prefers-color-scheme: dark)');
+    mediaList.addEventListener('change', changeTheme);
+
+    window.addEventListener('keydown', preventKeyScroll);
+
+    onDestroy(() => {
+      mediaList.removeEventListener('change', changeTheme);
+      window.removeEventListener('keydown', preventKeyScroll);
+    });
   });
 </script>
+
+<main>
+  <Bar />
+  <div class="content">
+    <ErrorGame />
+  </div>
+</main>
 
 <svelte:head>
   <title>ACM at CSUF / {$page.status || 404}</title>
 </svelte:head>
 
-{#if showGame}
-  <ErrorGame />
-{/if}
+<Footer />
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .content {
+    flex: 1; 
+  }
+</style>
