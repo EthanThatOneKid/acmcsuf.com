58298f35fd89fe33136cca5663f462940a667093
Evan Jimenez <evanmji1123@gmail.com>
2024-11-20 16:51:18 -0800

    Made table and workshop components; tested looping with components

--- src/routes/(site)/workshops/+page.svelte
+++ src/routes/(site)/workshops/+page.svelte
@@ -1,48 +1,21 @@
-<script>
+<script lang="ts">
   import Spacing from '$lib/public/legacy/spacing.svelte';
+  import TeamTable from './team-table.svelte';
+  import Workshop from './workshop.svelte';
+  let ws = [['hi', 'now', 'hi there'], ['bye', 'yesterday', 'bye there']];
+  let teams = ["OSS", "AI", "Dev", "Algo"];
 </script>
+
+
 
 <Spacing --min="125px" --med="150px" --max="150px" />
 
-<div class="hero-container">
-  <h1>Open Source Software Workshops</h1>
-  <br><br>
-  <table>
-    <tr>
-      <th>Workshop</th>
-      <th>Timestamp</th>
-      <th class="desc">Description</th>
-    </tr>
-    <tr>
-      <td>Intro to Java</td>
-      <td>3/14/24</td>
-      <td class="desc">Best language</td>
-    </tr>
-    <tr>
-      <td>Intro to VIM</td>
-      <td>2/29/24</td>
-      <td class="desc">Best Editor</td>
-    </tr>
-  </table>
-</div>
+{#each {length: 4} as _, i}
+  <TeamTable team={teams[i]} data={ws} />
+{/each}
+
+<Spacing --min="125px" --med="150px" --max="150px" />
 
 <style>
-  .hero-container {
-    margin: 40px;
-  }
-  table {
-    border: 3px white solid;
-    margin: auto;
-  }
-  th {
-    background-color: var(--acm-abyss);
-  }
-  th, td {
-    border: 3px gray solid;
-    padding: 7px;
-  }
-  .desc {
-    padding-left: 35px;
-    padding-right: 35px;
-  }
+  
 </style>
