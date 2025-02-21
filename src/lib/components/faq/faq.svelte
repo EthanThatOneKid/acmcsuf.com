f5908277300adb5890ce46b1c638dbff2c865cb5
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-02-20 21:30:56 -0800

    start base code for faq section; need to be redesigned
    TODO: ensure faq is responsive and works on dark/light mode

---
+++ src/lib/components/faq/faq.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let question: string;
+  export let answer: string;
+</script>
+
+<div class="container">
+  <h3>{question}</h3>
+  <p>{answer}</p>
+</div>
+
+<style>
+  /*Add temporary style, to be changed later*/
+  .container {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding: 16px;
+    border-radius: 8px;
+  }
+
+  h3 {
+    color: var(--acm-darker);
+  }
+
+  p {
+    color: var(--acm-darker);
+  }
+</style>
---
+++ src/routes/(site)/faq.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Data from '$lib/public/faq/data/faq.json';
+  import FAQ from '$lib/components/faq/faq.svelte';
+
+  export let faq = Data;
+</script>
+
+<div class="size-md">
+  <h1 class="hero-text">
+    <span>Frequently Asked Questions</span>
+  </h1>
+</div>
+{#each faq as { question, answer } (question)}
+  <FAQ {question} {answer} />
+  <p class="seperator" />
+{/each}
+
+<style>
+  .hero-text {
+    text-align: center;
+  }
+
+  .seperator {
+    margin: 8px 0px;
+    border-bottom: 1px solid var(--acm-blue);
+    width: 50%;
+    margin-inline: 16px;
+  }
+</style>
