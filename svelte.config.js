e294265ec35a5c7fbfdd27b98362802e18293a27
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-03-05 11:36:00 -0800

    svelte.config.js: Detect GitHub workflow
    This should allow the project to work in development, with Deno Deploy, and Netlify.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isGitHubWorkflow = process.env.GITHUB_ACTIONS === 'true';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +14,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: isGitHubWorkflow ? adapterNode() : adapterAuto(),
   },
 };
 
