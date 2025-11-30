af850195f3db3f91774d89e0ccd3febe5af6c372
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2025-11-30 11:15:14 -0800

    mobile view kinda works

--- src/routes/(site)/hero.svelte
+++ src/routes/(site)/hero.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
   import AcmButton from '$lib/components/button/button.svelte';
   const badges = [
-    { title: "General Team", src: "/assets/badge-acm.svg", href: "/teams#general" },
-    { title: "AI Team", src: "/assets/badge-ai.svg", href: "/teams#ai" },
-    { title: "Algo Team", src: "/assets/badge-algo.svg", href: "/teams#algo" },
-    { title: "Design Team", src: "/assets/badge-design.svg", href: "/teams#design" },
-    { title: "Dev Team", src: "/assets/badge-dev.svg", href: "/teams#dev" },
-    { title: "Game Dev Team", src: "/assets/badge-gamedev.svg", href: "/teams#gamedev" },
-    { title: "ICPC Team", src: "/assets/badge-icpc.svg", href: "/teams#icpc" },
-    { title: "Marketing Team", src: "/assets/badge-marketing.svg", href: "/teams#marketing" },
-    { title: "Node Buds Team", src: "/assets/badge-node-buds.svg", href: "/teams#nodebuds" },
-    { title: "Open Source Team", src: "/assets/badge-open-source.svg", href: "/teams#oss" },
+    { title: 'General Team', src: '/assets/badge-acm.svg', href: '/teams#general' },
+    { title: 'AI Team', src: '/assets/badge-ai.svg', href: '/teams#ai' },
+    { title: 'Algo Team', src: '/assets/badge-algo.svg', href: '/teams#algo' },
+    { title: 'Design Team', src: '/assets/badge-design.svg', href: '/teams#design' },
+    { title: 'Dev Team', src: '/assets/badge-dev.svg', href: '/teams#dev' },
+    { title: 'Game Dev Team', src: '/assets/badge-gamedev.svg', href: '/teams#gamedev' },
+    { title: 'ICPC Team', src: '/assets/badge-icpc.svg', href: '/teams#icpc' },
+    { title: 'Marketing Team', src: '/assets/badge-marketing.svg', href: '/teams#marketing' },
+    { title: 'Node Buds Team', src: '/assets/badge-node-buds.svg', href: '/teams#nodebuds' },
+    { title: 'Open Source Team', src: '/assets/badge-open-source.svg', href: '/teams#oss' },
   ];
 </script>
-
-
 
 <section class="hero-container">
   <div class="hero-inner-container">
@@ -27,12 +25,15 @@
         <AcmButton text="Join today!" link="/discord" redirect={true} />
       </div>
     </div>
-    
+
     <div class="orbit-container">
       <div class="capy-solo">
-        <img src="/assets/capy-power-solo.svg" alt="Capy the Capybara surrounded by all the ACM Team Badges" />
+        <img
+          src="/assets/capy-power-solo.svg"
+          alt="Capy the Capybara surrounded by all the ACM Team Badges"
+        />
       </div>
-    
+
       {#each badges as badge, i}
         <div class="orbit" style="--i:{i}">
           <a title={badge.title} href={badge.href}>
@@ -41,50 +42,49 @@
         </div>
       {/each}
     </div>
-
   </div>
 </section>
 
 <style>
-.orbit-container {
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.capy-solo img {
-  width: 250px;
-  height: 250px;
-  position: absolute;
-  transform: translate(-56%, -50%);
-}
-
-.orbit {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 70px;
-  height: 70px;
-  transform-origin: center center;
-  animation: spin 72s linear infinite;
-}
-
-.orbit img {
-  width: 100%;
-  height: 100%;
-  transform: translate(-50%, -50%);
-}
-
-/* Animation with correct counter-rotation */
-@keyframes spin {
-  0% {
-    transform: rotate(calc(var(--i) * 36deg)) translate(180px) rotate(calc(-1 * var(--i) * 36deg));
+  .orbit-container {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
-  100% {
-    transform: rotate(calc(var(--i) * 36deg + 360deg)) translate(180px) rotate(calc(-1 * (var(--i) * 36deg + 360deg)));
+
+  .capy-solo img {
+    position: absolute;
+    width: 150px;
+    height: 150px;
+    transform: translate(-56%, -50%);
   }
-}
+
+  .orbit {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 3em;
+    height: 3em;
+    transform-origin: center center;
+    animation: spin 72s linear infinite;
+  }
+
+  .orbit img {
+    width: 100%;
+    height: 100%;
+    transform: translate(-50%, -50%);
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(calc(var(--i) * 36deg)) translate(110px) rotate(calc(-1 * var(--i) * 36deg));
+    }
+    100% {
+      transform: rotate(calc(var(--i) * 36deg + 360deg)) translate(110px)
+        rotate(calc(-1 * (var(--i) * 36deg + 360deg)));
+    }
+  }
 
   section {
     display: grid;
@@ -127,7 +127,7 @@
     justify-self: center;
   }
 
-  section .hero-inner-container  {
+  section .hero-inner-container {
     max-width: 90%;
     justify-self: center;
     animation-name: spin;
@@ -159,5 +159,27 @@
     section .hero-inner-container .hero-text .join-button {
       justify-self: start;
     }
-  } 
+
+    .capy-solo img {
+      width: 250px;
+      height: 250px;
+      transform: translate(-56%, -50%);
+    }
+
+    .orbit {
+      width: 5em;
+      height: 5em;
+    }
+
+    @keyframes spin {
+      0% {
+        transform: rotate(calc(var(--i) * 36deg)) translate(180px)
+          rotate(calc(-1 * var(--i) * 36deg));
+      }
+      100% {
+        transform: rotate(calc(var(--i) * 36deg + 360deg)) translate(180px)
+          rotate(calc(-1 * (var(--i) * 36deg + 360deg)));
+      }
+    }
+  }
 </style>
